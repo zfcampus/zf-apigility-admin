f84cb55a648382981173fcfada65085b31d7b313
Ralph Schindler <ralph@ralphschindler.com>
2013-07-19 15:17:18 -0500

    Initial app and diagnostic REST view

--- asset/zf-api-first-admin/js/app.js
+++ asset/zf-api-first-admin/js/app.js
@@ -1,1 +1,22 @@
 
+'use strict';
+
+angular.module('zf-api-first-admin', [])
+    .controller('IndexController', [function() {
+
+    }])
+    .controller('DiagnosticsController', ['$http', '$scope', function($http, $scope) {
+        $http({method: 'GET', url: '/zftool/diagnostics'}).
+            success(function(data, status, headers, config) {
+                $scope.diagnostics = data;
+            }).
+            error(function(data, status, headers, config) {
+                console.log(data);
+            });
+    }])
+    .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+        $routeProvider.when('/', {templateUrl: 'partials/index.html', controller: 'IndexController'});
+        $routeProvider.when('/diagnostics', {templateUrl: 'partials/diagnostics.html', controller: 'DiagnosticsController'});
+    }])
+;
+
