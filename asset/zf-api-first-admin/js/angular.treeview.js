fe7e3ab87e0194ea6f1c1cbf05d51223dcd4390c
Enrico Zimuel <e.zimuel@gmail.com>
2013-08-09 19:06:06 +0200

    Added the first UI prototype

---
+++ asset/zf-api-first-admin/js/angular.treeview.js
@@ -0,0 +1,113 @@
+/*
+	@license Angular Treeview version 0.1.1
+	ⓒ 2013 AHN JAE-HA http://github.com/eu81273/angular.treeview
+	License: MIT
+
+
+	[TREE attribute]
+	angular-treeview: the treeview directive
+	tree-model : the tree model on $scope.
+	node-id : each node's id
+	node-label : each node's label
+	node-children: each node's children
+
+	<div
+		data-angular-treeview="true"
+		data-tree-model="roleList"
+		data-node-id="roleId"
+		data-node-label="roleName"
+		data-node-children="children" >
+	</div>
+*/
+
+(function ( angular ) {
+	'use strict';
+
+	angular.module( 'angularTreeview', [] ).directive( 'treeModel', function( $compile ) {
+		return {
+			restrict: 'A',
+			link: function ( scope, element, attrs ) {
+				//tree model
+				var treeModel = attrs.treeModel;
+
+				//node id
+				var nodeId = attrs.nodeId || 'id';
+
+				//node label
+				var nodeLabel = attrs.nodeLabel || 'label';
+
+				//children
+				var nodeChildren = attrs.nodeChildren || 'children';
+
+				//icon
+				var nodeIcon = attrs.nodeIcon || 'icon';
+				
+				//tree template
+				var template = 
+					'<ul>' + 
+						'<li data-ng-repeat="node in ' + treeModel + '" class="' + nodeIcon + '">' + 
+							'<i class="collapsed" data-ng-show="node.' + nodeChildren + '.length && node.collapsed" data-ng-click="selectNodeHead(node, $event)"></i>' + 
+							'<i class="expanded" data-ng-show="node.' + nodeChildren + '.length && !node.collapsed" data-ng-click="selectNodeHead(node, $event)"></i>' + 
+							'<i class="normal" data-ng-hide="node.' + nodeChildren + '.length"></i> ' + 
+							'<span data-ng-class="node.selected" data-ng-click="selectNodeLabel(node, $event)">{{node.' + nodeLabel + '}}</span>' + 
+							'<div data-ng-hide="node.collapsed" data-tree-model="node.' + nodeChildren + '" data-node-id=' + nodeId + ' data-node-label=' + nodeLabel + ' data-node-children=' + nodeChildren + ' data-node-icon=' + nodeIcon + '></div>' + 
+						'</li>' + 
+					'</ul>'; 
+
+
+				//check tree model
+				if( treeModel && treeModel.length ) {
+					//root node
+					if( attrs.angularTreeview ) {
+						//$watch tree model
+						scope.$watch( treeModel, function( _new, _old ) { 
+
+							//Rendering template.
+							element.empty().html( $compile( template )( scope ) );
+
+						}, false ); //if true, re-rendering the treeview every clicks.
+
+
+						//if node head clicks,
+						scope.selectNodeHead = scope.selectNodeHead || function( selectedNode, $event ){
+							//stop event bubbling 
+							if ( $event.stopPropagation ) $event.stopPropagation();
+							if ( $event.preventDefault ) $event.preventDefault();
+							$event.cancelBubble = true;
+							$event.returnValue = false;
+
+							//Collapse or Expand
+							selectedNode.collapsed = !selectedNode.collapsed;
+						};
+
+						//if node label clicks,
+						scope.selectNodeLabel = scope.selectNodeLabel || function( selectedNode, $event ){
+							//stop event bubbling 
+							if ( $event.stopPropagation ) $event.stopPropagation();
+							if ( $event.preventDefault ) $event.preventDefault();
+							$event.cancelBubble = true;
+							$event.returnValue = false;
+
+							//remove highlight from previous node
+							if( scope.currentNode && scope.currentNode.selected ) {
+								scope.currentNode.selected = undefined;
+							}
+
+							//set highlight to selected node
+							selectedNode.selected = 'selected'
+
+							//set currentNode
+							scope.currentNode = selectedNode;
+						};
+					}
+
+					//chlid nodes
+					else {
+						//Rendering template created.
+						element.html( $compile( template )( scope ) );
+					}
+				}
+			}
+		};
+	});
+})( angular );
--- asset/zf-api-first-admin/js/app.js
+++ asset/zf-api-first-admin/js/app.js
@@ -1,20 +1,97 @@
 
 'use strict';
 
-angular.module('zf-api-first-admin', [])
-    .controller('IndexController', [function() {
+angular.module('zf-api-first-admin', ['angularTreeview'])
+    .controller('IndexController', ['$http', '$scope', '$location', function($http, $scope, $location) {
+		
+        $scope.step1 = function() {
+			$location.path('/step2');
+		  };
+        
+        $scope.step2 = function() {
+			$location.path('/step3');
+		  };
+		  
+		  $scope.step3 = function() {
+			$location.path('/step4');
+		  };
+		  
+		  $scope.step4 = function() {
+		  	$location.path('/step5');
+		  };
+		  
+		  $scope.step5 = function() {
+		  	$location.path('/step6');
+		  };
+		  
+        $scope.loadConfiguration = function () {
+			var req = {
+				method: 'GET',
+                url: '/admin/api/config',
+                data: '',
+                headers: {
+                    'Content-Type': 'application/vnd.zfcampus.v1.config+json'
+                }
+			};
+            $http(req)
+                .success(function (data) {
+                   
+                   var result = buildTree(data,0);
+                   console.log(result);
+                   $scope.tree = result;
+                });
+        };
+
+
+        $scope.addDatabase = function() {
+            $http.get('/zf-api-first-admin/partials/configuration-db.html')
+            .success(function (data) {
+                $scope.configForm = data;
+            });
+        };
+
+        $scope.doSubmit = function () {
+            var req = {
+                method: 'PATCH',
+                url: '/admin/api/config',
+                headers: {
+                    'Accept': 'application/vnd.zfcampus.v1.config+json',
+                    'Content-Type': 'application/vnd.zfcampus.v1.config+json'
+                },
+                data: {db: $scope.db}
+            };
+            $http(req)
+                .success(function (data) {
+                    $scope.configForm = 'Configuration saved.';
+                    $scope.loadConfiguration();
+                });
+
+    	};
 
     }])
     .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-        $routeProvider.when('/', {templateUrl: '/zf-api-first-admin/partials/index.html', controller: 'IndexController'});
-        $routeProvider.when('/configuration', {templateUrl: '/zf-api-first-admin/partials/configuration.html', controller: 'ConfigurationController'});
+      $routeProvider.when('/', {templateUrl: '/zf-api-first-admin/partials/wizard-step-1.html', controller: 'IndexController'});
+      $routeProvider.when('/step2', {templateUrl: '/zf-api-first-admin/partials/wizard-step-2.html', controller: 'IndexController'});
+	   $routeProvider.when('/step3', {templateUrl: '/zf-api-first-admin/partials/wizard-step-3.html', controller: 'IndexController'});
+		$routeProvider.when('/step4', {templateUrl: '/zf-api-first-admin/partials/wizard-step-4.html', controller: 'IndexController'});
+		$routeProvider.when('/step5', {templateUrl: '/zf-api-first-admin/partials/wizard-step-5.html', controller: 'IndexController'});
+		$routeProvider.when('/step6', {templateUrl: '/zf-api-first-admin/partials/wizard-step-6.html', controller: 'IndexController'});
+		$routeProvider.when('/step7', {templateUrl: '/zf-api-first-admin/partials/wizard-step-7.html', controller: 'IndexController'});
+      $routeProvider.when('/configuration', {templateUrl: '/zf-api-first-admin/partials/configuration.html', controller: 'ConfigurationController'});
     }])
     .controller('ConfigurationController', ['$http', '$scope', '$compile', function($http, $scope, $compile) {
 
         $scope.loadConfiguration = function () {
-            $http.get('/admin/api/config')
+			var req = {
+				method: 'GET',
+                url: '/admin/api/config',
+                headers: {
+                    'Content-Type': 'application/vnd.zfcampus.v1.config+json'
+                }
+			};
+            $http(req)
                 .success(function (data) {
-                    $scope.configurations = data;
+                    $scope.tree = data;
                 });
         };
 
@@ -46,7 +123,7 @@ angular.module('zf-api-first-admin', [])
 
         $scope.loadConfiguration();
     }])
-    .directive('compile', function($compile) {
+	.directive('compile', function($compile) {
 	// directive factory creates a link function
 	return function(scope, element, attrs) {
 	    scope.$watch(
@@ -70,3 +147,15 @@ angular.module('zf-api-first-admin', [])
     })
 ;
 
+function buildTree(obj, count) {
+	var result = Array();
+	for (var key in obj) {
+		var value = obj[key];
+		if (value instanceof Object) {
+			result.push({ "name" : key, "id" : count++, "icon" : key, "child" : buildTree(value, count++) });
+		} else {
+			result.push({ "name" : key + " : " + value, "icon" : key, "id" : count++, "child" : [] })
+		}
+	}
+	return result;
+}
