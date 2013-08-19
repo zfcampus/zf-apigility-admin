5374203f52fbc03a395d0cc078e97fad5efac4f6
Ralph Schindler <ralph@ralphschindler.com>
2013-08-19 15:17:03 -0500

    Fixed tabs->spaces in js file

--- asset/zf-api-first-admin/js/app.js
+++ asset/zf-api-first-admin/js/app.js
@@ -5,9 +5,9 @@ var module = angular.module('zfa1-admin', ['HALParser']);
 module.controller(
     'IndexController',
     ['$rootScope', function($rootScope) {
-	$rootScope.pageTitle = 'Dashboard';
-	$rootScope.pageDescription = 'Global system configuration and configuration to be applied to all modules.';
-	$rootScope.subNavItems = ['General Information', 'Media Types', 'Authenticaiton', 'phpinfo()', 'ZF2 Info'];
+        $rootScope.pageTitle = 'Dashboard';
+        $rootScope.pageDescription = 'Global system configuration and configuration to be applied to all modules.';
+        $rootScope.subNavItems = ['General Information', 'Media Types', 'Authenticaiton', 'phpinfo()', 'ZF2 Info'];
     }]
 );
 
@@ -15,40 +15,38 @@ module.controller(
     'ModuleController',
     ['$rootScope', '$scope', '$routeParams', '$http', 'HALParser', function($rootScope, $scope, $routeParams, $http, HALParser) {
 
-	var halParser = new HALParser;
+        var halParser = new HALParser;
 
-	$rootScope.pageTitle = $routeParams.name;
-	$rootScope.pageDescription = '';
+        $rootScope.pageTitle = $routeParams.name;
+        $rootScope.pageDescription = '';
 
-	$scope.showRestResources = ($routeParams.section == 'rest-resources');
+        $scope.showRestResources = ($routeParams.section == 'rest-resources');
 
-	function load() {
-	    $http.get('/admin/api/module/' + $routeParams.name)
-		.success(function (data) {
-//                    console.log(data);
-		    $scope.moduleResource = halParser.parse(data);
-		    $rootScope.pageTitle = $scope.moduleResource.namespace;
-		    $rootScope.pageDescription = '';
+        function load() {
+            $http.get('/admin/api/module/' + $routeParams.name)
+            .success(function (data) {
 
-		    $rootScope.subNavItems = {};
-		    $rootScope.subNavItems['module/' + $scope.moduleResource.module + '/info'] = 'General Information';
-		    $rootScope.subNavItems['module/' + $scope.moduleResource.module + '/rest-resources'] = 'API Resources';
-		    $rootScope.subNavItems['module/' + $scope.moduleResource.module + '/api-endpoints'] = 'API RPC Endpoints';
-		    $rootScope.subNavItems['module/' + $scope.moduleResource.module + '/authentication'] = 'Authentication';
-		    $rootScope.subNavItems['module/' + $scope.moduleResource.module + '/filters-validators'] = 'Filters / Validatators';
+                $scope.moduleResource = halParser.parse(data);
+                $rootScope.pageTitle = $scope.moduleResource.namespace;
+                $rootScope.pageDescription = '';
 
-		});
-	}
+                $rootScope.subNavItems = {};
+                $rootScope.subNavItems['module/' + $scope.moduleResource.module + '/info'] = 'General Information';
+                $rootScope.subNavItems['module/' + $scope.moduleResource.module + '/rest-resources'] = 'API Resources';
+                $rootScope.subNavItems['module/' + $scope.moduleResource.module + '/api-endpoints'] = 'API RPC Endpoints';
+                $rootScope.subNavItems['module/' + $scope.moduleResource.module + '/authentication'] = 'Authentication';
+                $rootScope.subNavItems['module/' + $scope.moduleResource.module + '/filters-validators'] = 'Filters / Validatators';
 
-	load();
+            });
+        }
+
+        load();
     }]
 );
 
 module.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.when('/', {templateUrl: '/zf-api-first-admin/partials/index.html', controller: 'IndexController'});
     $routeProvider.when('/module/:name/:section', {templateUrl: '/zf-api-first-admin/partials/module.html', controller: 'ModuleController'});
-    // Configure the app to use push state routing.
-//    $locationProvider.html5Mode(true).hashPrefix('#');
 }]);
 
 module.run(['$rootScope', '$http', 'HALParser', function ($rootScope, $http, HALParser) {
@@ -58,11 +56,10 @@ module.run(['$rootScope', '$http', 'HALParser', function ($rootScope, $http, HAL
     var halParser = new HALParser;
 
     function initialize() {
-	$http.get('/admin/api/module')
-	    .success(function (data) {
-//                console.log(data);
-		$rootScope.moduleResources = halParser.parse(data);
-	    });
+        $http.get('/admin/api/module')
+            .success(function (data) {
+                $rootScope.moduleResources = halParser.parse(data);
+            });
     }
 
     initialize();
