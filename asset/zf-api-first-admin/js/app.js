2b35350c227235d75d2f57c34f6141e9b1efec68
Ralph Schindler <ralph@ralphschindler.com>
2013-08-19 15:08:05 -0500

    Updated angular front end to talk to endpoints for nav and controller list

--- asset/zf-api-first-admin/js/app.js
+++ asset/zf-api-first-admin/js/app.js
@@ -1,12 +1,71 @@
-
 'use strict';
 
-angular.module('zf-api-first-admin')
-    .controller('IndexController', ['$http', '$scope', '$location', function($http, $scope, $location) {
+var module = angular.module('zfa1-admin', ['HALParser']);
 
-    }])
-    .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-	$routeProvider.when('/', {templateUrl: '/zf-api-first-admin/partials/index.html', controller: 'IndexController'});
-    }])
-;
+module.controller(
+    'IndexController',
+    ['$rootScope', function($rootScope) {
+	$rootScope.pageTitle = 'Dashboard';
+	$rootScope.pageDescription = 'Global system configuration and configuration to be applied to all modules.';
+	$rootScope.subNavItems = ['General Information', 'Media Types', 'Authenticaiton', 'phpinfo()', 'ZF2 Info'];
+    }]
+);
 
+module.controller(
+    'ModuleController',
+    ['$rootScope', '$scope', '$routeParams', '$http', 'HALParser', function($rootScope, $scope, $routeParams, $http, HALParser) {
+
+	var halParser = new HALParser;
+
+	$rootScope.pageTitle = $routeParams.name;
+	$rootScope.pageDescription = '';
+
+	$scope.showRestResources = ($routeParams.section == 'rest-resources');
+
+	function load() {
+	    $http.get('/admin/api/module/' + $routeParams.name)
+		.success(function (data) {
+//                    console.log(data);
+		    $scope.moduleResource = halParser.parse(data);
+		    $rootScope.pageTitle = $scope.moduleResource.namespace;
+		    $rootScope.pageDescription = '';
+
+		    $rootScope.subNavItems = {};
+		    $rootScope.subNavItems['module/' + $scope.moduleResource.module + '/info'] = 'General Information';
+		    $rootScope.subNavItems['module/' + $scope.moduleResource.module + '/rest-resources'] = 'API Resources';
+		    $rootScope.subNavItems['module/' + $scope.moduleResource.module + '/api-endpoints'] = 'API RPC Endpoints';
+		    $rootScope.subNavItems['module/' + $scope.moduleResource.module + '/authentication'] = 'Authentication';
+		    $rootScope.subNavItems['module/' + $scope.moduleResource.module + '/filters-validators'] = 'Filters / Validatators';
+
+		});
+	}
+
+	load();
+    }]
+);
+
+module.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider.when('/', {templateUrl: '/zf-api-first-admin/partials/index.html', controller: 'IndexController'});
+    $routeProvider.when('/module/:name/:section', {templateUrl: '/zf-api-first-admin/partials/module.html', controller: 'ModuleController'});
+    // Configure the app to use push state routing.
+//    $locationProvider.html5Mode(true).hashPrefix('#');
+}]);
+
+module.run(['$rootScope', '$http', 'HALParser', function ($rootScope, $http, HALParser) {
+
+    $rootScope.moduleResources = [];
+
+    var halParser = new HALParser;
+
+    function initialize() {
+	$http.get('/admin/api/module')
+	    .success(function (data) {
+//                console.log(data);
+		$rootScope.moduleResources = halParser.parse(data);
+	    });
+    }
+
+    initialize();
+
+
+}]);
