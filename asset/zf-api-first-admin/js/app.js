26046b729ebd7205f572d7c6efebb23e7f33bca7
Ralph Schindler <ralph@ralphschindler.com>
2013-09-13 12:35:57 -0500

    Further refactor module->api terminilogy

--- asset/zf-api-first-admin/js/app.js
+++ asset/zf-api-first-admin/js/app.js
@@ -1,45 +1,45 @@
 'use strict';
 
-var module = angular.module('zfa1-admin', []);
+var module = angular.module('ag-admin', []);
 
 module.controller(
     'DashboardController',
     ['$rootScope', function($rootScope) {
         $rootScope.pageTitle = 'Dashboard';
-        $rootScope.pageDescription = 'Global system configuration and configuration to be applied to all modules.';
+        $rootScope.pageDescription = 'Global system configuration and configuration to be applied to all APIs.';
     }]
 );
 
 module.controller(
-    'ModuleListController',
-    ['$rootScope', '$scope', '$location', 'ModulesResource', function($rootScope, $scope, $location, ModulesResource) {
-        // $scope vars
-        $scope.modules = [];
+    'APIListController',
+    ['$rootScope', '$scope', '$location', 'APIsResource', function($rootScope, $scope, $location, APIsResource) {
 
-        $scope.createNewModule = function () {
-            ModulesResource.createNewModule($scope.moduleName).then(function (newModule) {
-                ModulesResource.fetch({force: true}).then(function (modules) {
+        $scope.apis = [];
+
+        $scope.createNewAPI = function () {
+            APIsResource.createNewAPI($scope.apiName).then(function (newAPI) {
+                APIsResource.fetch({force: true}).then(function (apis) {
                     $scope.resetForm();
-                    updateModuleList();
-                    $location.path('/module/' + newModule.name + '/info');
+                    updateAPIList();
+                    $location.path('/api/' + newAPI.name + '/info');
                 });
             });
         };
 
         $scope.resetForm = function () {
-            $scope.addModule = false;
-            $scope.moduleName = '';
-        }
+            $scope.addAPI = false;
+            $scope.apiName = '';
+        };
 
-        var updateModuleList = function () {
-            ModulesResource.fetch().then(function (modules) {
+        var updateAPIList = function () {
+            APIsResource.fetch().then(function (apis) {
                 $scope.$apply(function () {
-                    $scope.modules = modules.embedded.module;
+                    $scope.apis = apis.embedded.module;
                 });
             });
         };
 
-        updateModuleList();
+        updateAPIList();
     }]
 );
 
@@ -49,10 +49,10 @@ module.controller(
     ['$rootScope', '$scope', '$routeParams', 'SecondaryNavigationService', function ($rootScope, $scope, $routeParams, SecondaryNavigationService) {
 
         function updateSecondaryNavigation() {
-            if ($routeParams.moduleName == undefined) {
+            if ($routeParams.apiName == undefined) {
                 $scope.items = SecondaryNavigationService.getGlobalNavigation();
             } else {
-                $scope.items = SecondaryNavigationService.getModuleNavigation($routeParams.moduleName);
+                $scope.items = SecondaryNavigationService.getAPINavigation($routeParams.apiName);
                 $scope.section = $routeParams.section;
             }
         }
@@ -66,62 +66,55 @@ module.controller(
 );
 
 module.controller(
-    'ModuleController',
-    ['$rootScope', '$scope', '$routeParams', 'ModulesResource', function($rootScope, $scope, $routeParams, ModulesResource) {
+    'APIController',
+    ['$rootScope', '$scope', '$routeParams', 'APIsResource', function($rootScope, $scope, $routeParams, APIsResource) {
 
-        $scope.module = null;
+        $scope.api = null;
         $scope.section = null;
 
-        function updateModule() {
+        APIsResource.fetch().then(function (apis) {
 
-            ModulesResource.fetch().then(function (modules) {
-
-                var briefModule = _.find(modules.embedded.module, function (m) {
-                    return m.props.name === $routeParams.moduleName;
-                });
-
-                briefModule.links['self'].fetch().then(function (module) {
-                    // update UI immediately:
-                    $scope.$apply(function () {
-                        $scope.module = module;
-                        $rootScope.pageTitle = module.props.namespace;
-                        $rootScope.pageDescription = 'tbd';
-                        $scope.section = $routeParams.section;
-                    });
-                });
-
+            // @todo matthew claims this is the full-enough resource now, perhaps refactor
+            var briefModule = _.find(apis.embedded.module, function (m) {
+                return m.props.name === $routeParams.apiName;
             });
-        }
-        updateModule();
 
-        $scope.$on('Module.refresh', function () {
-            updateModule();
+            briefModule.links['self'].fetch().then(function (api) {
+                // update UI immediately:
+                $scope.$apply(function () {
+                    $scope.api = api;
+                    $rootScope.pageTitle = api.props.namespace;
+                    $rootScope.pageDescription = 'tbd';
+                    $scope.section = $routeParams.section;
+                });
+            });
+
         });
 
     }]
 );
 
-module.directive('moduleRestEndpoints', function () {
+module.directive('apiRestEndpoints', function () {
     return {
         restrict: 'E',
-        templateUrl: '/zf-api-first-admin/partials/module/rest-endpoints.html',
-        controller: ['$rootScope', '$scope', 'ModulesResource', function ($rootScope, $scope, ModulesResource) {
-            $scope.module = $scope.$parent.module;
+        templateUrl: '/zf-api-first-admin/partials/api/rest-endpoints.html',
+        controller: ['$rootScope', '$scope', 'APIsResource', function ($rootScope, $scope, APIsResource) {
+            $scope.api = $scope.$parent.api;
 
-            function updateModuleRestEndpoints(force) {
+            function updateAPIRestEndpoints(force) {
                 $scope.restEndpoints = [];
-                $scope.module.links['rest'].fetch({force: force}).then(function (restEndpoints) {
+                $scope.api.links['rest'].fetch({force: force}).then(function (restEndpoints) {
                     // update view
                     $scope.$apply(function() {
                         $scope.restEndpoints = restEndpoints.embedded.rest;
                     });
                 });
             }
-            updateModuleRestEndpoints(false);
+            updateAPIRestEndpoints(false);
 
             $scope.createNewRestEndpoint = function () {
-                ModulesResource.createNewRestEndpoint($scope.module.props.name, $scope.restEndpointName).then(function (restResource) {
-                    updateModuleRestEndpoints(true);
+                APIsResource.createNewRestEndpoint($scope.api.props.name, $scope.restEndpointName).then(function (restResource) {
+                    updateAPIRestEndpoints(true);
                     $scope.addRestEndpoint = false;
                     $scope.restEndpointName = '';
                 });
@@ -130,27 +123,27 @@ module.directive('moduleRestEndpoints', function () {
     }
 });
 
-module.directive('moduleRpcEndpoints', function () {
+module.directive('apiRpcEndpoints', function () {
     return {
         restrict: 'E',
-        templateUrl: '/zf-api-first-admin/partials/module/rpc-endpoints.html',
-        controller: ['$rootScope', '$scope', 'ModulesResource', function ($rootScope, $scope, ModulesResource) {
-            $scope.module = $scope.$parent.module;
+        templateUrl: '/zf-api-first-admin/partials/api/rpc-endpoints.html',
+        controller: ['$rootScope', '$scope', 'APIsResource', function ($rootScope, $scope, APIsResource) {
+            $scope.api = $scope.$parent.api;
 
-            function updateModuleRpcEndpoints(force) {
+            function updateAPIRpcEndpoints(force) {
                 $scope.rpcEndpoints = [];
-                $scope.module.links['rpc'].fetch({force: force}).then(function (rpcEndpoints) {
+                $scope.api.links['rpc'].fetch({force: force}).then(function (rpcEndpoints) {
                     // update view
                     $scope.$apply(function() {
                         $scope.rpcEndpoints = rpcEndpoints.embedded.rpc;
                     });
                 });
             }
-            updateModuleRpcEndpoints(false);
+            updateAPIRpcEndpoints(false);
 
             $scope.createNewRpcEndpoint = function () {
-                ModulesResource.createNewRpcEndpoint($scope.module.props.name, $scope.rpcEndpointName, $scope.rpcEndpointRoute).then(function (rpcResource) {
-                    updateModuleRpcEndpoints(true);
+                APIsResource.createNewRpcEndpoint($scope.api.props.name, $scope.rpcEndpointName, $scope.rpcEndpointRoute).then(function (rpcResource) {
+                    updateAPIRpcEndpoints(true);
                     $scope.addRpcEndpoint = false;
                     $scope.rpcEndpointName = '';
                     $scope.rpcEndpointRoute = '';
@@ -162,11 +155,13 @@ module.directive('moduleRpcEndpoints', function () {
 
 module.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.when('/dashboard', {templateUrl: '/zf-api-first-admin/partials/index.html', controller: 'DashboardController'});
-    $routeProvider.when('/module/:moduleName/:section', {templateUrl: '/zf-api-first-admin/partials/module.html', controller: 'ModuleController'});
+    $routeProvider.when('/api/:apiName/:section', {templateUrl: '/zf-api-first-admin/partials/api.html', controller: 'APIController'});
     $routeProvider.otherwise({redirectTo: '/dashboard'})
 }]);
 
 module.factory('SecondaryNavigationService', function () {
+
+    // @todo if this is not shared anymore, this should move to the appropriate controller
     return {
         getGlobalNavigation: function () {
             return [
@@ -177,37 +172,37 @@ module.factory('SecondaryNavigationService', function () {
                 {id: 'zf2info', name: "zf2info()", link: '/zf2info'}
             ];
         },
-        getModuleNavigation: function (moduleName, section) {
+        getAPINavigation: function (apiName, section) {
             return [
-                {id: 'info', name: "General Information", link: '/module/' + moduleName + '/info'},
-                {id: 'rest-endpoints', name: "REST Endpoints", link: '/module/' + moduleName + '/rest-endpoints'},
-                {id: 'rpc-endpoints', name: "RPC Endpoints", link: '/module/' + moduleName + '/rpc-endpoints'},
-                {id: 'authentication', name: "Authentication", link: '/module/' + moduleName + '/authentication'},
-                {id: 'filters-validators', name: "Filters / Validators", link: '/module/' + moduleName + '/filters-validators'}
+                {id: 'info', name: "General Information", link: '/api/' + apiName + '/info'},
+                {id: 'rest-endpoints', name: "REST Endpoints", link: '/api/' + apiName + '/rest-endpoints'},
+                {id: 'rpc-endpoints', name: "RPC Endpoints", link: '/api/' + apiName + '/rpc-endpoints'},
+                {id: 'authentication', name: "Authentication", link: '/api/' + apiName + '/authentication'},
+                {id: 'filters-validators', name: "Filters / Validators", link: '/api/' + apiName + '/filters-validators'}
             ];
         }
     };
 });
 
-module.factory('ModulesResource', ['$http', function ($http) {
+module.factory('APIsResource', ['$http', function ($http) {
     var resource = new Hyperagent.Resource('/admin/api/module');
 
-    resource.createNewModule = function (name) {
+    resource.createNewAPI = function (name) {
         return $http.post('/admin/api/module', {name: name})
             .then(function (response) {
                 return response.data;
             });
     };
 
-    resource.createNewRestEndpoint = function (moduleName, name) {
-        return $http.post('/admin/api/module/' + moduleName + '/rest', {resource_name: name})
+    resource.createNewRestEndpoint = function (apiName, restEndpointName) {
+        return $http.post('/admin/api/module/' + apiName + '/rest', {resource_name: restEndpointName})
             .then(function (response) {
                 return response.data;
             });
     };
 
-    resource.createNewRpcEndpoint = function (moduleName, name, route) {
-        return $http.post('/admin/api/module/' + moduleName + '/rpc', {service_name: name, route: route})
+    resource.createNewRpcEndpoint = function (apiName, rpcEndpointName, rpcEndpointRoute) {
+        return $http.post('/admin/api/module/' + apiName + '/rpc', {service_name: rpcEndpointName, route: rpcEndpointRoute})
             .then(function (response) {
                 return response.data;
             });
