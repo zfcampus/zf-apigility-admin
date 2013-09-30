4e25f941486040d30d8c270c40b6422612e70200
Matthew Weier O'Phinney <matthew@zend.com>
2013-09-30 16:18:30 -0500

    Ensure we return ApiProblemModels from RPC controllers

--- src/ZF/Apigility/Admin/Controller/VersioningController.php
+++ src/ZF/Apigility/Admin/Controller/VersioningController.php
@@ -9,6 +9,7 @@ use Zend\Mvc\Controller\AbstractActionController;
 use ZF\Apigility\Admin\Exception;
 use ZF\Apigility\Admin\Model\VersioningModelFactory;
 use ZF\ApiProblem\ApiProblem;
+use ZF\ApiProblem\View\ApiProblemModel;
 
 class VersioningController extends AbstractActionController
 {
@@ -25,7 +26,9 @@ class VersioningController extends AbstractActionController
 
         $module = $this->bodyParam('module', false);
         if (!$module) {
-            return new ApiProblem(422, 'Module parameter not provided', 'https://tools.ietf.org/html/rfc4918', 'Unprocessable Entity');
+            return new ApiProblemModel(
+                new ApiProblem(422, 'Module parameter not provided', 'https://tools.ietf.org/html/rfc4918', 'Unprocessable Entity')
+            );
         }
 
         $model = $this->modelFactory->factory($module);
@@ -34,7 +37,7 @@ class VersioningController extends AbstractActionController
         if (!$version) {
             $versions = $model->getModuleVersions($module);
             if (!$versions) {
-                return new ApiProblem(404, 'Module not found');
+                return new ApiProblemModel(new ApiProblem(404, 'Module not found'));
             }
             sort($versions);
             $version = array_pop($versions);
@@ -45,7 +48,9 @@ class VersioningController extends AbstractActionController
         try {
             $result = $model->createVersion($module, $version);
         } catch (Exception\InvalidArgumentException $ex) {
-            return new ApiProblem(422, 'Invalid module and/or version', 'https://tools.ietf.org/html/rfc4918', 'Unprocessable Entity');
+            return new ApiProblemModel(
+                new ApiProblem(422, 'Invalid module and/or version', 'https://tools.ietf.org/html/rfc4918', 'Unprocessable Entity')
+            );
         }
 
         return array(
