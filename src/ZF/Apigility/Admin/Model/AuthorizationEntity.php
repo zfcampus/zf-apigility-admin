94fe481f1a98cb22c8039c40f47003fa4d2bfb62
Matthew Weier O'Phinney <matthew@zend.com>
2013-11-07 15:46:33 -0600

    Created AuthorizationEntity
    - Allows composing REST and RPC services along with the various
      authorization privileges based on HTTP methods.

---
+++ src/ZF/Apigility/Admin/Model/AuthorizationEntity.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2013 Zend Technologies USA Inc. (http://www.zend.com)
+ */
+
+namespace ZF\Apigility\Admin\Model;
+
+use ArrayIterator;
+use IteratorAggregate;
+use ZF\Apigility\Admin\Exception;
+
+class AuthorizationEntity implements IteratorAggregate
+{
+    const TYPE_RESOURCE   = 'resource';
+    const TYPE_COLLECTION = 'collection';
+
+    protected $allowedRestTypes = array(
+        self::TYPE_RESOURCE,
+        self::TYPE_COLLECTION,
+    );
+
+    protected $defaultPrivileges = array(
+        'GET'    => false,
+        'POST'   => false,
+        'PATCH'  => false,
+        'PUT'    => false,
+        'DELETE' => false,
+    );
+
+    protected $servicePrivileges = array();
+
+    public function __construct(array $services = array())
+    {
+        foreach ($services as $serviceName => $privileges) {
+            $this->servicePrivileges[$serviceName] = $this->filterPrivileges($privileges);
+        }
+    }
+
+    public function getIterator()
+    {
+        return new ArrayIterator($this->servicePrivileges);
+    }
+
+    public function addRestService($serviceName, $resourceOrCollection, array $privileges = null)
+    {
+        if (!in_array($resourceOrCollection, $this->allowedRestTypes)) {
+            throw new Exception\InvalidArgumentException(sprintf(
+                'Invalid type "%s" provided for %s; must be one of "%s" or "%s"',
+                $resourceOrCollection,
+                __METHOD__,
+                self::TYPE_RESOURCE,
+                self::TYPE_COLLECTION
+            ));
+        }
+        $this->addRpcService($serviceName, sprintf('__%s__', $resourceOrCollection), $privileges);
+        return $this;
+    }
+
+    public function addRpcService($serviceName, $action, array $privileges = null)
+    {
+        if (null === $privileges) {
+            $privileges = $this->defaultPrivileges;
+        }
+
+        $serviceName = sprintf('%s::%s', $serviceName, $action);
+        $this->servicePrivileges[$serviceName] = $this->filterPrivileges($privileges);
+        return $this;
+    }
+
+    public function has($serviceName)
+    {
+        return array_key_exists($serviceName, $this->servicePrivileges);
+    }
+
+    public function get($serviceName)
+    {
+        if (!$this->has($serviceName)) {
+            throw new Exception\InvalidArgumentException(sprintf(
+                'No service by the name of "%s" has been registered',
+                $serviceName
+            ));
+        }
+        return $this->servicePrivileges[$serviceName];
+    }
+
+    protected function filterPrivileges(array $privileges)
+    {
+        foreach ($privileges as $httpMethod => $flag) {
+            if (!array_key_exists($httpMethod, $this->defaultPrivileges)) {
+                unset($privileges[$httpMethod]);
+                continue;
+            }
+            if (!is_bool($flag)) {
+                $privileges[$httpMethod] = (bool) $flag;
+                continue;
+            }
+        }
+        return $privileges;
+    }
+}
---
+++ test/ZFTest/Apigility/Admin/Model/AuthorizationEntityTest.php
@@ -0,0 +1,183 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2013 Zend Technologies USA Inc. (http://www.zend.com)
+ */
+
+namespace ZFTest\Apigility\Admin\Model;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use ZF\Apigility\Admin\Model\AuthorizationEntity;
+
+class AuthorizationEntityTest extends TestCase
+{
+    protected function getSeedValuesForEntity()
+    {
+        return array(
+            'Foo\V1\Rest\Session\Controller::__resource__' => array(
+                'GET' => true,
+                'POST' => true,
+                'PATCH' => true,
+                'PUT' => false,
+                'DELETE' => false,
+            ),
+            'Foo\V1\Rest\Session\Controller::__collection__' => array(
+                'GET' => true,
+                'POST' => false,
+                'PATCH' => false,
+                'PUT' => false,
+                'DELETE' => false,
+            ),
+            'Foo\V1\Rpc\Message\Controller::message' => array(
+                'GET' => true,
+                'POST' => true,
+                'PATCH' => false,
+                'PUT' => false,
+                'DELETE' => false,
+            ),
+            'Foo\V1\Rpc\Message\Controller::translate' => array(
+                'GET' => true,
+                'POST' => true,
+                'PATCH' => false,
+                'PUT' => false,
+                'DELETE' => false,
+            ),
+        );
+    }
+
+    public function testEntityIsIterable()
+    {
+        $values = $this->getSeedValuesForEntity();
+        $entity = new AuthorizationEntity($values);
+        $this->assertInstanceOf('Traversable', $entity);
+    }
+
+    public function testIteratingEntityReturnsAKeyForEachOfRestResourceAndCollection()
+    {
+        $values = $this->getSeedValuesForEntity();
+        $entity = new AuthorizationEntity($values);
+
+        $keys = array();
+        foreach ($entity as $key => $value) {
+            $keys[] = $key;
+        }
+        $this->assertContains('Foo\V1\Rest\Session\Controller::__resource__', $keys);
+        $this->assertContains('Foo\V1\Rest\Session\Controller::__collection__', $keys);
+    }
+
+    public function testIteratingEntityReturnsAKeyForEachActionOfRpcController()
+    {
+        $values = $this->getSeedValuesForEntity();
+        $entity = new AuthorizationEntity($values);
+
+        $keys = array();
+        foreach ($entity as $key => $value) {
+            $keys[] = $key;
+        }
+        $this->assertContains('Foo\V1\Rpc\Message\Controller::message', $keys);
+        $this->assertContains('Foo\V1\Rpc\Message\Controller::translate', $keys);
+    }
+
+    public function testCanAddARestServiceAtATime()
+    {
+        $entity = new AuthorizationEntity();
+        $entity->addRestService('Foo\V1\Rest\Session\Controller', AuthorizationEntity::TYPE_RESOURCE, array(
+            'GET' => true,
+            'POST' => true,
+            'PATCH' => true,
+            'PUT' => false,
+            'DELETE' => false,
+        ));
+        $entity->addRestService('Foo\V1\Rest\Session\Controller', AuthorizationEntity::TYPE_COLLECTION, array(
+            'GET' => true,
+            'POST' => false,
+            'PATCH' => false,
+            'PUT' => false,
+            'DELETE' => false,
+        ));
+
+        $keys = array();
+        foreach ($entity as $key => $value) {
+            $keys[] = $key;
+        }
+        $this->assertContains('Foo\V1\Rest\Session\Controller::__resource__', $keys);
+        $this->assertContains('Foo\V1\Rest\Session\Controller::__collection__', $keys);
+    }
+
+    public function testCanAddAnRpcServiceAtATime()
+    {
+        $entity = new AuthorizationEntity();
+        $entity->addRpcService('Foo\V1\Rpc\Message\Controller', 'message', array(
+            'GET' => true,
+            'POST' => true,
+            'PATCH' => false,
+            'PUT' => false,
+            'DELETE' => false,
+        ));
+        $entity->addRpcService('Foo\V1\Rpc\Message\Controller', 'translate', array(
+            'GET' => true,
+            'POST' => true,
+            'PATCH' => false,
+            'PUT' => false,
+            'DELETE' => false,
+        ));
+
+        $keys = array();
+        foreach ($entity as $key => $value) {
+            $keys[] = $key;
+        }
+        $this->assertContains('Foo\V1\Rpc\Message\Controller::message', $keys);
+        $this->assertContains('Foo\V1\Rpc\Message\Controller::translate', $keys);
+    }
+
+    public function testCanRetrieveNamedServices()
+    {
+        $entity = new AuthorizationEntity();
+        $entity->addRpcService('Foo\V1\Rpc\Message\Controller', 'message', array(
+            'GET' => true,
+            'POST' => true,
+            'PATCH' => false,
+            'PUT' => false,
+            'DELETE' => false,
+        ));
+        $this->assertTrue($entity->has('Foo\V1\Rpc\Message\Controller::message'));
+        $privileges = $entity->get('Foo\V1\Rpc\Message\Controller::message');
+        $this->assertEquals(array(
+            'GET' => true,
+            'POST' => true,
+            'PATCH' => false,
+            'PUT' => false,
+            'DELETE' => false,
+        ), $privileges);
+    }
+
+    public function testAddingARestServiceWithoutHttpMethodsProvidesDefaults()
+    {
+        $entity = new AuthorizationEntity();
+        $entity->addRestService('Foo\V1\Rest\Session\Controller', AuthorizationEntity::TYPE_RESOURCE);
+        $this->assertTrue($entity->has('Foo\V1\Rest\Session\Controller::__resource__'));
+        $privileges = $entity->get('Foo\V1\Rest\Session\Controller::__resource__');
+        $this->assertEquals(array(
+            'GET' => false,
+            'POST' => false,
+            'PATCH' => false,
+            'PUT' => false,
+            'DELETE' => false,
+        ), $privileges);
+    }
+
+    public function testAddingAnRpcServiceWithoutHttpMethodsProvidesDefaults()
+    {
+        $entity = new AuthorizationEntity();
+        $entity->addRpcService('Foo\V1\Rpc\Message\Controller', 'message');
+        $this->assertTrue($entity->has('Foo\V1\Rpc\Message\Controller::message'));
+        $privileges = $entity->get('Foo\V1\Rpc\Message\Controller::message');
+        $this->assertEquals(array(
+            'GET' => false,
+            'POST' => false,
+            'PATCH' => false,
+            'PUT' => false,
+            'DELETE' => false,
+        ), $privileges);
+    }
+}
