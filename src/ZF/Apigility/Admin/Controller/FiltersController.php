0a49f98e3a78d75f5d8995e3fea8c59e18bb6b99
Matthew Weier O'Phinney <matthew@zend.com>
2014-01-06 15:45:34 -0600

    Created controller and factory for filters service
    - Created controller
    - Created corresponding factory
    - Added model and controller factories to configuration

---
+++ src/ZF/Apigility/Admin/Controller/FiltersController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2013 Zend Technologies USA Inc. (http://www.zend.com)
+ */
+
+namespace ZF\Apigility\Admin\Controller;
+
+use ZF\Apigility\Admin\Model\FiltersModel;
+
+class FiltersController extends AbstractPluginManagerController
+{
+    protected $property = 'filters';
+
+    public function __construct(FiltersModel $model)
+    {
+        $this->model = $model;
+    }
+
+    public function filtersAction()
+    {
+        return $this->handleRequest();
+    }
+}
