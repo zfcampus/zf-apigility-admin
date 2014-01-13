064f4e7bffe523263541b2e7197fee55519f842d
Matthew Weier O'Phinney <matthew@zend.com>
2014-01-13 17:50:08 -0600

    Enable nav
    - Use controller from currently matched route to determine active
      navigation item.
    - Simplify markup for navbar, and ensure that active item is visibly
      active.

--- asset/zf-apigility-admin/js/app.js
+++ asset/zf-apigility-admin/js/app.js
@@ -1183,10 +1183,11 @@ module.filter('namespaceclassid', function () {
     };
 });
 
-module.run(['$rootScope', '$routeParams', '$location', function ($rootScope, $routeParams, $location) {
+module.run(['$rootScope', '$routeParams', '$location', '$route', function ($rootScope, $routeParams, $location, $route) {
     $rootScope.routeParams = $routeParams;
 
     $rootScope.$on('$routeChangeSuccess', function(scope, next, current){
+        scope.targetScope.$root.navSection = $route.current.controller;
         if (next.locals.api && scope.targetScope.$root.pageTitle != next.locals.api.name) {
             scope.targetScope.$root.pageTitle = next.locals.api.name;
         }
