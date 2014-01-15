5323d8d17bae2c0442d828d41779f1fab4719416
Matthew Weier O'Phinney <matthew@zend.com>
2014-01-15 15:55:02 -0600

    Began view/edit paradigm in REST services
    - New directives, ag-tabs and ag-tab-pane
    - Use collapse UI to provide buttons for view vs edit on each service
        - View by default
            - Shows settings and source code
            - Will show fields and docs
        - Click edit button:
            - Shows edit forms for settings and fields
            - Will show docs

--- asset/zf-apigility-admin/js/app.js
+++ asset/zf-apigility-admin/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var module = angular.module('ag-admin', ['ngRoute', 'ngSanitize', 'tags-input', 'angular-flash.service', 'angular-flash.flash-alert-directive', 'ui.sortable', 'ui.select2', 'ag-collapse', 'ag-hover', 'ag-include', 'ag-edit-inplace']);
+var module = angular.module('ag-admin', ['ngRoute', 'ngSanitize', 'tags-input', 'angular-flash.service', 'angular-flash.flash-alert-directive', 'ui.sortable', 'ui.select2', 'ag-collapse', 'ag-hover', 'ag-include', 'ag-edit-inplace', 'ag-tabs']);
 
 module.config(['$routeProvider', '$provide', function($routeProvider, $provide) {
 
