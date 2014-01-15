7069e70bd72504e47f0bcff2ed0d4b7ae8d36140
Matthew Weier O'Phinney <matthew@zend.com>
2014-01-15 14:48:01 -0600

    "edit-in-place" feature of fields
    - Created directive to implement "edit in place" feature so that it will
      interact with the collapse header correctly.

--- asset/zf-apigility-admin/js/app.js
+++ asset/zf-apigility-admin/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var module = angular.module('ag-admin', ['ngRoute', 'ngSanitize', 'tags-input', 'angular-flash.service', 'angular-flash.flash-alert-directive', 'ui.sortable', 'ui.select2', 'ag-collapse', 'ag-hover', 'ag-include']);
+var module = angular.module('ag-admin', ['ngRoute', 'ngSanitize', 'tags-input', 'angular-flash.service', 'angular-flash.flash-alert-directive', 'ui.sortable', 'ui.select2', 'ag-collapse', 'ag-hover', 'ag-include', 'ag-edit-inplace']);
 
 module.config(['$routeProvider', '$provide', function($routeProvider, $provide) {
 
