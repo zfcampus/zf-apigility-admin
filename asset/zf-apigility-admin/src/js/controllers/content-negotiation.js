e99f53ba73f2457613c619e2e4d1ad56e827c165
Matthew Weier O'Phinney <matthew@zend.com>
2014-01-31 10:02:41 -0600

    Completed content-negotiation UI
    - Adds view/edit/delete capabilities.

--- asset/zf-apigility-admin/src/js/controllers/content-negotiation.js
+++ asset/zf-apigility-admin/src/js/controllers/content-negotiation.js
@@ -2,7 +2,7 @@
 
 angular.module('ag-admin').controller(
   'ContentNegotiationController',
-  ['$scope', '$location', 'flash', 'selectors', function ($scope, $location, flash, selectors) {
+  ['$scope', '$location', 'flash', 'selectors', 'ContentNegotiationResource', function ($scope, $location, flash, selectors, ContentNegotiationResource) {
     var newSelector = {
       content_name: '',
       viewModel: '',
@@ -11,7 +11,7 @@ angular.module('ag-admin').controller(
 
     $scope.showNewSelectorForm = false;
     $scope.newSelector = _.cloneDeep(newSelector);
-    $scope.selectors = selectors;
+    $scope.selectors = _.cloneDeep(selectors);
 
     $scope.resetNewSelectorForm = function() {
       $scope.showNewSelectorForm = false;
@@ -23,11 +23,71 @@ angular.module('ag-admin').controller(
       $scope.newSelector.viewModel = '';
     };
 
+    $scope.removeViewModel = function (viewModel, selector) {
+      delete selector.selectors[viewModel];
+    };
+
+    $scope.resetSelectorForm = function (selector) {
+      /* Reset to original values */
+      var name = selector.content_name;
+      var originalSelector;
+      angular.forEach(selectors, function (value) {
+        if (originalSelector || value.content_name !== name) {
+          return;
+        }
+        originalSelector = value;
+      });
+      if (! originalSelector) {
+        return;
+      }
+      angular.forEach($scope.selectors, function (value, key) {
+        if (value.content_name !== originalSelector.content_name) {
+          return;
+        }
+        $scope.selectors[key] = originalSelector;
+      });
+    };
+
     $scope.createSelector = function() {
       delete $scope.newSelector.viewModel;
-      console.log($scope.newSelector);
-      flash.success = 'New selector created';
-      $scope.resetNewSelectorForm();
+
+      ContentNegotiationResource.createSelector($scope.newSelector).then(function (selector) {
+        selectors.push(selector);
+        $scope.selectors.push(selector);
+        flash.success = 'New selector created';
+        $scope.resetNewSelectorForm();
+      });
+    };
+
+    $scope.updateSelector = function (selector) {
+      delete selector.viewModel;
+      
+      ContentNegotiationResource.updateSelector(selector).then(function (updated) {
+        /* Update original selector on success, so that view matches */
+        var updatedSelector = false;
+        angular.forEach(selectors, function (value, key) {
+          if (updatedSelector || value.content_name !== updated.content_name) {
+            return;
+          }
+          selectors[key] = updated;
+          updatedSelector = true;
+        });
+
+        flash.success = 'Selector updated';
+      });
+
+    };
+
+    $scope.removeSelector = function (selectorName) {
+      ContentNegotiationResource.removeSelector(selectorName).then(function () {
+        flash.success = 'Selector removed';
+
+        ContentNegotiationResource.getList().then(function (updatedSelectors) {
+          selectors = updatedSelectors;
+          $scope.selectors = _.cloneDeep(selectors);
+        });
+
+      });
     };
   }]
 );
--- asset/zf-apigility-admin/src/js/services/content-negotiation.js
+++ asset/zf-apigility-admin/src/js/services/content-negotiation.js
@@ -8,6 +8,18 @@ angular.module('ag-admin').factory(
     var servicePath = apiBasePath + '/content-negotiation';
 
     return {
+      prepareSelector: function (selector) {
+        var data = {
+          content_name: selector.content_name
+        };
+
+        angular.forEach(selector.selectors, function (value, key) {
+          data[key] = value;
+        });
+
+        return data;
+      },
+
       getList: function () {
         return $http({method: 'GET', url: servicePath}).then(
           function success(response) {
@@ -15,6 +27,60 @@ angular.module('ag-admin').factory(
           },
           function error() {
             flash.error = 'Unable to fetch content negotiation selectors; you may need to reload the page';
+          }
+        );
+      },
+
+      createSelector: function (selector) {
+        return $http({
+          method: 'POST',
+          url: servicePath,
+          data: this.prepareSelector(selector)
+        }).then(
+          function success(response) {
+            return response.data;
+          },
+          function error(response) {
+            flash.error = 'Unable to create selector; please try again';
+            return response;
+          }
+        );
+      },
+
+      updateSelector: function (selector) {
+        var updatePath = servicePath + '/' + encodeURIComponent(selector.content_name);
+
+        var data = this.prepareSelector(selector);
+        delete data.content_name;
+
+        return $http({
+          method: 'PATCH',
+          url: updatePath,
+          data: data
+        }).then(
+          function success(response) {
+            return response.data;
+          },
+          function error(response) {
+            flash.error = 'Unable to create selector; please try again';
+            return response;
+          }
+        );
+      },
+
+      removeSelector: function (selectorName) {
+        var updatePath = servicePath + '/' + encodeURIComponent(selectorName);
+
+        return $http({
+          method: 'DELETE',
+          url: updatePath
+        }).then(
+          function success(response) {
+            return response.data;
+          },
+          function error(response) {
+            flash.error = 'Unable to remove selector; please try again';
+            return response;
           }
         );
       }
