09182fb825bdfb3511f146a9e6e879b20db44eee
Matthew Weier O'Phinney <matthew@zend.com>
2014-02-02 13:56:08 -0600

    Fixed API creation logic
    As reported on the ML, the "create API" form was not actually submitting. This
    was due to a change in the template which prevented the modal from having the
    same scope as the API list in the navbar.
    
    This patch separates the creation vs list logic into separate controllers, and
    has the creation logic emit a signal that the list logic intercepts so that the
    navbar will dynamically update on creation.

--- asset/zf-apigility-admin/src/js/controllers/api-list.js
+++ asset/zf-apigility-admin/src/js/controllers/api-list.js
@@ -2,36 +2,12 @@
 
 angular.module('ag-admin').controller(
     'ApiListController',
-    ['$rootScope', '$scope', '$location', '$timeout', 'flash', 'ApiRepository', function($rootScope, $scope, $location, $timeout, flash, ApiRepository) {
+    ['$rootScope', '$scope', 'ApiRepository', function($rootScope, $scope, ApiRepository) {
 
         $scope.apis = [];
-        $scope.showNewApiForm = false;
 
         $scope.refreshApiList = function () {
             ApiRepository.getList(true).then(function (apis) { $scope.apis = apis; });
-        };
-
-        $scope.createNewApi = function ($event) {
-            var form = angular.element($event.target);
-            form.find('input').attr('disabled', true);
-            form.find('button').attr('disabled', true);
-
-            ApiRepository.createNewApi($scope.apiName).then(function (newApi) {
-                // reset form, repopulate, redirect to new
-                $scope.dismissModal();
-                $scope.resetForm();
-                $scope.refreshApiList();
-
-                flash.success = 'New API Created';
-                $timeout(function () {
-                    $location.path('/api/' + newApi.name + '/v1/overview');
-                }, 500);
-            });
-        };
-
-        $scope.resetForm = function () {
-            $scope.showNewApiForm = false;
-            $scope.apiName = '';
         };
 
         $rootScope.$on('refreshApiList', function () { $scope.refreshApiList(); });
