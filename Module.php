78e17b0d4f8691e1dec63bdf650d67f8b7a2ae62
Matthew Weier O'Phinney <matthew@zend.com>
2014-02-12 11:05:21 -0600

    Fix line 397
    - s/entity/halEntity/, as the latter can actually have links...

--- Module.php
+++ Module.php
@@ -394,7 +394,7 @@ class Module
             || $entity instanceof Model\RpcServiceEntity
             || (is_array($entity) && array_key_exists('controller_service_name', $entity))
         ) {
-            $links = $entity->getLinks();
+            $links = $halEntity->getLinks();
 
             if ($links->has('input_filter')) {
                 $serviceName = is_array($entity) ? $entity['controller_service_name'] : $entity->controllerServiceName;
