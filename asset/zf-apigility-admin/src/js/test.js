b4dcf29cea714349fc201e276ab65d87458bd396
Matthew Weier O'Phinney <matthew@zend.com>
2014-02-19 11:11:21 -0600

    Working ui-router skeleton
    Includes:
    
    - navbar, with active selection based on active state
    - breadcrumbs, based on state hierarchy
    - page title, updated based on state
    - sidebar, chosen based on state, and with active selection based on state
    - content, based on state
    
    CSS was updated to ensure navbar and breadcrumbs display correctly.

---
+++ asset/zf-apigility-admin/src/js/test.js
@@ -0,0 +1,202 @@
+(function() {
+    'use strict';
+
+    /**
+     * Declare and configure modules
+     */
+    angular.module('ag-test', [
+        'ui.router',
+        'ngSanitize'
+    ]).config(
+        function($provide, $stateProvider, $urlRouterProvider) {
+            // setup the API Base Path (this should come from initial ui load/php)
+            $provide.value('apiBasePath', angular.element('body').data('api-base-path') || '/admin/api');
+
+            $stateProvider.state('ag', {
+                abstract: true,
+                views: {
+                    breadcrumbs: {
+                        templateUrl: 'html/breadcrumbs.html',
+                        controller: ['$scope', '$state', '$stateParams', function ($scope, $state, $stateParams) {
+                            $scope.breadcrumbs = [];
+
+                            var home = {
+                                title: 'Home',
+                                href: 'ag.settings.overview',
+                                active: false
+                            };
+
+                            var generateBreadcrumb = function (state) {
+                                if (state.abstract || !state.url || !state.data || !state.data.pageTitle) {
+                                    return false;
+                                }
+
+                                return {
+                                    title: state.data.pageTitle,
+                                    href: $state.url,
+                                    active: false
+                                };
+                            };
+
+                            var createBreadcrumbs = function () {
+                                var breadcrumbs = [];
+                                var active = $state.$current.name;
+
+                                var state = $state.$current;
+                                while (state.name !== 'ag') {
+                                    var breadcrumb = generateBreadcrumb(state);
+
+                                    if (breadcrumb) {
+                                        if (state.name === active) {
+                                            breadcrumb.active = true;
+                                        }
+                                        breadcrumbs.unshift(breadcrumb);
+                                    }
+
+                                    if (!state.parent) {
+                                        break;
+                                    }
+
+                                    state = state.parent;
+                                }
+
+                                breadcrumbs.unshift(home);
+
+                                var uris = [];
+                                breadcrumbs = breadcrumbs.filter(function (breadcrumb) {
+                                    if (uris.lastIndexOf(breadcrumb.href) === -1) {
+                                        uris.push(breadcrumb.href);
+                                        return true;
+                                    }
+
+                                    return false;
+                                });
+
+                                $scope.breadcrumbs = breadcrumbs;
+                                $scope.params = $stateParams;
+                            };
+
+                            $scope.$watch(function () {
+                                return $state.current.name;
+                            }, createBreadcrumbs);
+
+                            createBreadcrumbs();
+                        }]
+                    },
+                    title: {
+                        template: '<h1 ng-bind="pageTitle"></h1>',
+                        controller: ['$scope', '$state', function ($scope, $state) {
+
+                            var update = function (oldval, newval) {
+                                if (oldval === newval) {
+                                    return;
+                                }
+
+                                var pageTitle;
+                                if ($state.current && $state.current.data && $state.current.data.pageTitle) {
+                                    pageTitle = $state.current.data.pageTitle;
+                                }
+                                $scope.pageTitle = pageTitle;
+                            };
+
+                            $scope.$watch(function () {
+                                return $state.current.name;
+                            }, update);
+
+                            update($state);
+                        }]
+                    },
+                    sidebar: {
+                        templateUrl: 'html/settings/sidebar.html',
+                        controller: ['$scope', '$state', function ($scope, $state) {
+                            var update = function () {
+                                $scope.active = $state.current.name;
+                            };
+
+                            $scope.$watch(function () {
+                                return $state.current.name;
+                            }, update);
+                            update();
+                        }]
+                    }
+                }
+            });
+            $stateProvider.state('ag.settings', {
+                abstract: true,
+                url: '/settings'
+            });
+            $stateProvider.state('ag.settings.overview', {
+                url: '/overview',
+                data: {
+                    pageTitle: 'Settings'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'html/settings/dashboard.html'
+                    }
+                }
+            });
+            $stateProvider.state('ag.settings.authentication', {
+                url: '/authentication',
+                data: {
+                    pageTitle: 'Authentication'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'html/settings/authentication.html'
+                    }
+                }
+            });
+            $stateProvider.state('ag.settings.content-negotiation', {
+                url: '/content-negotiation',
+                data: {
+                    pageTitle: 'Content Negotiation'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'html/settings/content-negotiation.html'
+                    }
+                }
+            });
+            $stateProvider.state('ag.settings.db-adapters', {
+                url: '/db-adapters',
+                data: {
+                    pageTitle: 'Database Adapters'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'html/settings/db-adapters.html'
+                    }
+                }
+            });
+
+            $stateProvider.state('ag.api', {
+                url: '/api',
+                data: {
+                    pageTitle: 'APIs'
+                },
+                views: {
+                    'content@': {
+                        template: '<h4>APIs</h4>'
+                    },
+                    'sidebar@': {
+                        templateUrl: 'html/api/sidebar.html',
+                        controller: ['$scope', '$state', function ($scope, $state) {
+                            var update = function () {
+                                $scope.active = $state.current.name;
+                            };
+
+                            $scope.$watch(function () {
+                                return $state.current.name;
+                            }, update);
+                            update();
+                        }]
+                    }
+                }
+            });
+
+            $urlRouterProvider.otherwise('/settings/overview');
+        }
+    );
+
+})();
