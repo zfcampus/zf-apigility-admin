73b0d7ffbec9ac8703ea3ea33f248dc1bcc3f2da
Ralph Schindler <ralph@ralphschindler.com>
2014-03-13 17:47:20 -0500

    Initial changes to config to support full list of input-filters

---
+++ src/ZF/Apigility/Admin/Validator/ModuleNameValidator.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ZF\Apigility\Admin\Validator;
+
+class ModuleNameValidator extends AbstractValidator
+{
+    const API_NAME = 'api_name';
+
+    protected $messageTemplates = array(
+        self::API_NAME => "'%value%' is not a valid api name"
+    );
+
+    public function isValid($value)
+    {
+        $this->setValue($value);
+
+        if (!$this->isValidWordInPhp($value)) {
+            $this->error(self::API_NAME);
+            return false;
+        }
+
+        return true;
+    }
+}
