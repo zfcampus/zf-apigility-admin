d1dd235a783ccfae6e30aac47fbe1819797a779c
Ralph Schindler <ralph@ralphschindler.com>
2014-03-13 13:04:03 -0500

    CS fixes

--- src/ZF/Apigility/Admin/Validator/ModuleName.php
+++ src/ZF/Apigility/Admin/Validator/ModuleName.php
@@ -5,12 +5,12 @@ use Zend\Validator\AbstractValidator;
 
 class ModuleName extends AbstractValidator
 {
-    const MODULENAME = 'modulename';
+    const MODULE_NAME = 'module_name';
 
     protected $reservedWords = array('__halt_compiler', 'abstract', 'and', 'array', 'as', 'break', 'callable', 'case', 'catch', 'class', 'clone', 'const', 'continue', 'declare', 'default', 'die', 'do', 'echo', 'else', 'elseif', 'empty', 'enddeclare', 'endfor', 'endforeach', 'endif', 'endswitch', 'endwhile', 'eval', 'exit', 'extends', 'final', 'for', 'foreach', 'function', 'global', 'goto', 'if', 'implements', 'include', 'include_once', 'instanceof', 'insteadof', 'interface', 'isset', 'list', 'namespace', 'new', 'or', 'print', 'private', 'protected', 'public', 'require', 'require_once', 'return', 'static', 'switch', 'throw', 'trait', 'try', 'unset', 'use', 'var', 'while', 'xor');
 
     protected $messageTemplates = array(
-        self::MODULENAME => "'%value%' is not a valid module name"
+        self::MODULE_NAME => "'%value%' is not a valid module name"
     );
 
     public function isValid($value)
@@ -19,7 +19,7 @@ class ModuleName extends AbstractValidator
 
         if (!preg_match('/^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*$/', $value) ||
             in_array(strtolower($value), $this->reservedWords)) {
-                $this->error(self::MODULENAME);
+                $this->error(self::MODULE_NAME);
                 return false;
         }
 
