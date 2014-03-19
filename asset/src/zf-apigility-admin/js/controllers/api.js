37960388586a533cff0f6f5ebf17c0c4aeabd706
Matthew Weier O'Phinney <matthew@zend.com>
2014-03-19 17:24:47 -0500

    ag-form directive
    - Disables form inputs on form submission
    - Listens to "ag-form-submit-complete" event, and re-enables form inputs
    - Listens to "ag-form-validation-errors" event, and injects error messages into form
    - Listens to "ag-form-validation-errors-clear" event, and removes error messages
      from form, if any
    - Tested initially with "create api" modal form

--- asset/src/zf-apigility-admin/js/controllers/api.js
+++ asset/src/zf-apigility-admin/js/controllers/api.js
@@ -23,26 +23,49 @@ angular.module('ag-admin').controller(
       var form = angular.element($event.target);
       form.find('input').attr('disabled', true);
       form.find('button').attr('disabled', true);
+      form.find('.ag-validation-error').remove();
 
-      ApiRepository.createNewApi($scope.apiName).then(function (newApi) {
-        // reset form, repopulate, redirect to new
-        $scope.dismissModal();
-        $scope.resetForm();
-        form.find('input').attr('disabled', false);
-        form.find('button').attr('disabled', false);
+      ApiRepository.createNewApi($scope.apiName).then(
+        function (newApi) {
+          // reset form, repopulate, redirect to new
+          $scope.dismissModal();
+          $scope.$broadcast('ag-form-submit-complete');
+          $scope.resetForm();
 
-        flash.success = 'New API Created';
+          flash.success = 'New API Created';
 
-        ApiRepository.getList(true).then(function(apiCollection) {
-          updateList(apiCollection);
-          $state.go('ag.api.version', {apiName: newApi.name, version: 1});
-        });
-      });
+          ApiRepository.getList(true).then(function (apiCollection) {
+            updateList(apiCollection);
+            $state.go('ag.api.version', {apiName: newApi.name, version: 1});
+          });
+        },
+        function (error) {
+          $scope.$broadcast('ag-form-submit-complete');
+
+          if (error.status !== 400 && error.status !== 422) {
+            /* generic, non-validation related error! */
+            flash.error = 'Error submitting new API';
+            return;
+          }
+
+          var validationErrors;
+
+          if (error.status === 400) {
+            validationErrors = [ 'Unexpected or missing data processing form' ];
+          } else {
+            validationErrors = error.data.validation_messages;
+          }
+
+          $scope.$broadcast('ag-form-validation-errors', validationErrors);
+          flash.error = 'We were unable to validate your form; please check for errors.';
+        }
+      );
     };
 
     $scope.resetForm = function () {
       $scope.showNewApiForm = false;
       $scope.apiName = '';
+      $scope.$broadcast('ag-form-validation-errors-clear');
     };
 
     updateList(apis);
