49f407ceefcc50ac83bd3222dadf75efebaad583
Ralph Schindler <ralph@ralphschindler.com>
2014-03-19 12:06:19 -0500

    DbAdapterInputFilter test data added

--- test/InputFilter/DbAdapterInputFilterTest.php
+++ test/InputFilter/DbAdapterInputFilterTest.php
@@ -19,7 +19,11 @@ class DbAdapterInputFilterTest extends TestCase
 
     public function dataProviderIsValidTrue()
     {
-        return array();
+        return array(
+            array(
+                array('adapter_name' => 'Db\Status', 'database' => '/path/to/foobar', 'driver' => 'pdo_sqlite')
+            )
+        );
     }
 
     /**
@@ -35,7 +39,23 @@ class DbAdapterInputFilterTest extends TestCase
 
     public function dataProviderIsValidFalse()
     {
-        return array();
+        return array(
+            // adapter_name must be present
+            array(
+                array('database' => '/path/to/foobar', 'driver' => 'pdo_sqlite'),
+                array('adapter_name' => array('isEmpty' => 'Value is required and can\'t be empty'))
+            ),
+            // database must be present
+            array(
+                array('adapter_name' => 'Db\Status', 'driver' => 'pdo_sqlite'),
+                array('database' => array('isEmpty' => 'Value is required and can\'t be empty'))
+            ),
+            // driver must be present
+            array(
+                array('adapter_name' => 'Db\Status', 'database' => '/path/to/foobar'),
+                array('driver' => array('isEmpty' => 'Value is required and can\'t be empty'))
+            ),
+        );
     }
 }
  
