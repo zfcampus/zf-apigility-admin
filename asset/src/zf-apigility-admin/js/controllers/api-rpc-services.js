f910d1ca74da6e676067f5d8b7ca2e38c89f9a89
Matthew Weier O'Phinney <matthew@zend.com>
2014-03-20 08:47:42 -0500

    Add ag-form to new RPC service form
    - Incorporate ag-form into new RPC service form

--- asset/src/zf-apigility-admin/js/controllers/api-rpc-services.js
+++ asset/src/zf-apigility-admin/js/controllers/api-rpc-services.js
@@ -19,6 +19,8 @@ angular.module('ag-admin').controller(
     $scope.deleteRpcService = false;
 
     $scope.resetForm = function () {
+        $scope.$broadcast('ag-form-submit-complete');
+        $scope.$broadcast('ag-form-validation-errors-clear');
         $scope.showNewRpcServiceForm = false;
         $scope.rpcServiceName = '';
         $scope.rpcServiceRoute = '';
@@ -34,12 +36,33 @@ angular.module('ag-admin').controller(
 
 
     $scope.createNewRpcService = function () {
-        ApiRepository.createNewRpcService($scope.api.name, $scope.rpcServiceName, $scope.rpcServiceRoute)
-            .then(function (rpcResource) {
+        ApiRepository.createNewRpcService($scope.api.name, $scope.rpcServiceName, $scope.rpcServiceRoute).then(
+            function (rpcResource) {
                 $scope.addRpcService = false;
                 $scope.resetForm();
                 ApiRepository.refreshApi($scope, $state, true, 'New RPC Service created');
-            });
+            },
+            function (error) {
+                $scope.$broadcast('ag-form-submit-complete');
+
+                if (error.status !== 400 && error.status !== 422) {
+                    /* generic, non-validation related error! */
+                    flash.error = 'Error submitting new API';
+                    return;
+                }
+
+                var validationErrors;
+
+                if (error.status === 400) {
+                    validationErrors = [ 'Unexpected or missing data processing form' ];
+                } else {
+                    validationErrors = error.data.validation_messages;
+                }
+
+                $scope.$broadcast('ag-form-validation-errors', validationErrors);
+                flash.error = 'We were unable to validate your form; please check for errors.';
+            }
+        );
     };
 
     $scope.cancelEdit = function () {
