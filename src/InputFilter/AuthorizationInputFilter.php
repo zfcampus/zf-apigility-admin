7599eb16635dfb437bde1d2b27b62b9d6c6e1dec
Matthew Weier O'Phinney <matthew@zend.com>
2014-03-21 08:55:43 -0500

    Reviewed Authorization input filter
    - CS fixes
    - Test normalization
    - Refactor to extend InputFilter (simpler implementation)

--- src/InputFilter/AuthorizationInputFilter.php
+++ src/InputFilter/AuthorizationInputFilter.php
@@ -6,18 +6,11 @@
 
 namespace ZF\Apigility\Admin\InputFilter;
 
-use Zend\InputFilter\InputFilterInterface;
+use Zend\InputFilter\InputFilter;
 
-class AuthorizationInputFilter implements InputFilterInterface
+class AuthorizationInputFilter extends InputFilter
 {
-
-    protected $data;
     protected $messages = array();
-
-    public function setData($data)
-    {
-        $this->data = $data;
-    }
 
     /**
      * Is the data set valid?
@@ -31,19 +24,19 @@ class AuthorizationInputFilter implements InputFilterInterface
         foreach ($this->data as $className => $httpMethods) {
             // validate the structure of the controller service name / method
             if (strpos($className, '::') === false) {
-                $this->messages[$className]['invalidClassName'] = 'Class service name is invalid, must be serviceName::method';
+                $this->messages[$className][] = 'Class service name is invalid, must be serviceName::method, serviceName::__collection__, or serviceName::__entity__';
                 $isValid = false;
             }
 
             if (!is_array($httpMethods)) {
-                $this->messages[$className]['invalidHttpMethod'] = 'Values for each controller must be an http method keyd array of true/false values';
+                $this->messages[$className][] = 'Values for each controller must be an http method keyed array of true/false values';
                 $isValid = false;
                 continue;
             }
 
             foreach ($httpMethods as $httpMethod => $isRequired) {
                 if (!in_array($httpMethod, array('GET', 'POST', 'PUT', 'PATCH', 'DELETE'))) {
-                    $this->messages[$className]['invalidHttpMethod'] = 'Invalid header (' . $httpMethod . ') provided.';
+                    $this->messages[$className][] = 'Invalid HTTP method (' . $httpMethod . ') provided.';
                     $isValid = false;
                 }
             }
@@ -52,62 +45,11 @@ class AuthorizationInputFilter implements InputFilterInterface
         return $isValid;
     }
 
-    public function getRawValues()
-    {
-        return $this->data;
-    }
-
+    /**
+     * @return array
+     */
     public function getMessages()
     {
         return $this->messages;
     }
-
-    /**#@+
-     * Unnecessary methods required by interface for the purposes of this input filter
-     * @return void
-     */
-    public function count()
-    {
-    }
-
-    public function add($input, $name = null)
-    {
-    }
-
-    public function get($name)
-    {
-    }
-
-    public function has($name)
-    {
-    }
-
-    public function remove($name)
-    {
-    }
-
-    public function setValidationGroup($name)
-    {
-    }
-
-    public function getInvalidInput()
-    {
-    }
-
-    public function getValidInput()
-    {
-    }
-
-    public function getValue($name)
-    {
-    }
-
-    public function getValues()
-    {
-    }
-
-    public function getRawValue($name)
-    {
-    }
-    /**#@-*/
 }
--- test/InputFilter/AuthorizationInputFilterTest.php
+++ test/InputFilter/AuthorizationInputFilterTest.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2014 Zend Technologies USA Inc. (http://www.zend.com)
+ */
 
 namespace ZFTest\Apigility\Admin\InputFilter;
 
@@ -7,74 +11,69 @@ use ZF\Apigility\Admin\InputFilter\AuthorizationInputFilter;
 
 class AuthorizationInputFilterTest extends TestCase
 {
-    /**
-     * @dataProvider dataProviderIsValidTrue
-     */
-    public function testIsValidTrue($data)
-    {
-        $i = new AuthorizationInputFilter;
-        $i->setData($data);
-        $this->assertTrue($i->isValid());
-    }
-
-    public function dataProviderIsValidTrue()
+    public function dataProviderIsValid()
     {
         return array(
-            // empty
-            array(
-                array()
+            'empty' => array(
+                array(),
             ),
-            // with values
-            array(
+            'valid' => array(
                 array(
                     'Foo\V1\Rest\Bar\Controller::__entity__' => array('POST' => true, 'GET' => false),
-                    'Foo\V1\Rpc\Boom\Controller::boom' => array('GET' => true, 'DELETE' => false, 'PATCH' => true)
-                )
-            )
+                    'Foo\V1\Rpc\Boom\Controller::boom' => array('GET' => true, 'DELETE' => false, 'PATCH' => true),
+                ),
+            ),
         );
     }
 
-    /**
-     * @dataProvider dataProviderIsValidFalse
-     */
-    public function testIsValidFalse($data, $messages)
-    {
-        $i = new AuthorizationInputFilter;
-        $i->setData($data);
-        $this->assertFalse($i->isValid());
-        $this->assertEquals($messages, $i->getMessages());
-    }
-
-    public function dataProviderIsValidFalse()
+    public function dataProviderIsInvalid()
     {
         return array(
-            // invalid controller name
-            array(
+            'invalid-controller-name' => array(
                 array(
                     'Foo\V1\Rest\Bar\Controller' => array(),
                 ),
                 array(
-                    'Foo\V1\Rest\Bar\Controller' => array('invalidClassName' => 'Class service name is invalid, must be serviceName::method')
-                )
+                    'Foo\V1\Rest\Bar\Controller' => array('Class service name is invalid, must be serviceName::method, serviceName::__collection__, or serviceName::__entity__'),
+                ),
             ),
-            // not an array for values
-            array(
+            'values-not-array' => array(
                 array(
                     'Foo\V1\Rest\Bar\Controller::__entity__' => 'GET=true',
                 ),
                 array(
-                    'Foo\V1\Rest\Bar\Controller::__entity__' => array('invalidHttpMethod' => 'Values for each controller must be an http method keyd array of true/false values')
-                )
+                    'Foo\V1\Rest\Bar\Controller::__entity__' => array('Values for each controller must be an http method keyed array of true/false values'),
+                ),
             ),
-            // Invalid HTTP method
-            array(
+            'invalid-http-method' => array(
                 array(
                     'Foo\V1\Rest\Bar\Controller::__entity__' => array('MYMETHOD' => true),
                 ),
                 array(
-                    'Foo\V1\Rest\Bar\Controller::__entity__' => array('invalidHttpMethod' => 'Invalid header (MYMETHOD) provided.')
-                )
+                    'Foo\V1\Rest\Bar\Controller::__entity__' => array('Invalid HTTP method (MYMETHOD) provided.'),
+                ),
             ),
         );
+    }
+
+    /**
+     * @dataProvider dataProviderIsValid
+     */
+    public function testIsValid($data)
+    {
+        $filter = new AuthorizationInputFilter;
+        $filter->setData($data);
+        $this->assertTrue($filter->isValid());
+    }
+
+    /**
+     * @dataProvider dataProviderIsInvalid
+     */
+    public function testIsInvalid($data, $messages)
+    {
+        $filter = new AuthorizationInputFilter;
+        $filter->setData($data);
+        $this->assertFalse($filter->isValid());
+        $this->assertEquals($messages, $filter->getMessages());
     }
 }
