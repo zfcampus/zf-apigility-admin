e41c6a04aeaedb647fb2561387943cc61285e21e
Matthew Weier O'Phinney <matthew@zend.com>
2014-04-02 08:20:28 -0500

    Submit name change on enter
    - When a name change occurs, and the user presses the "enter" key, validate it
      and update the view.
    - Fixed validation logic in input filter controller; was always invalid, as it
      was checking the submitted name against... the submitted name!
    - Modified ag-edit-inplace to use ag-on-enter for the input; required pushing
      the submit logic into the scope.
    - Removed debug statements from ag-on-enter directive.

--- asset/src/zf-apigility-admin/js/directives/ag-on-enter.js
+++ asset/src/zf-apigility-admin/js/directives/ag-on-enter.js
@@ -5,23 +5,14 @@ angular.module('ag-admin').directive('agOnEnter',
         return {
             restrict: 'A',
             link: function(scope, element, attr) {
-console.log('Linking element on enter');
-console.log(element);
-console.log(attr.agOnEnter);
                 element.bind('keydown', function (e) {
-console.log('Detected keydown event');
-console.log(e);
                     element.focus();
                 });
 
                 element.bind('keyup', function (e) {
-console.log('Detected keyup event');
-console.log(e);
                     if (e.which !== 13) {
-console.log('Not an enter key');
                         return;
                     }
-console.log('Enter key detected; evaluating expression');
                     e.stopPropagation();
                     scope.$apply(attr.agOnEnter);
                 });
