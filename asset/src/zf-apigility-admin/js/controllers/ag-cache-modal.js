3bd16320e663486b59b87e2d8509181576240ed1
Matthew Weier O'Phinney <matthew@zend.com>
2014-04-07 08:52:14 -0500

    Refactored cache/fs permissions modals
    - Removed directives, and replaced with controllers. Controllers invoke the
      appropriate service, and, if necessary, open a modal using `$modal`. The
      modals are marked to not allow closing in any way (keyboard, clicking outside
      modal, no buttons present).
    - Removed extraneous markup from modal templates.
    - Updated index page to load controllers instead of directives, and to define
      ng-controller directive markup.

---
+++ asset/src/zf-apigility-admin/js/controllers/ag-cache-modal.js
@@ -0,0 +1,21 @@
+(function() { 'use strict';
+
+angular.module('ag-admin').controller(
+    'agCacheModal', 
+    function($modal, CacheEnabledResource) {
+        CacheEnabledResource.getCacheStatus().then(function (status) {
+            if (! status) {
+                return;
+            }
+
+            $modal.open({
+                templateUrl: 'html/modals/cache-check.html',
+                keyboard: false,
+                backdrop: 'static'
+            });
+        });
+    }
+);
+
+})();
+
