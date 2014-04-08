a8d955b65ae3d3a799965cdd0409eb6c8b40f678
Matthew Weier O'Phinney <matthew@zend.com>
2014-04-08 12:00:37 -0500

    [#142] Resolves RPC controller versioning issue
    - Versioning was not considering the "controllers" configuration, and, as such,
      was not versioning RPC controller service names. This led to 2 situations:
    
      - The controller service name for the new version of the RPC service did not
        exist in configuration, meaning it could not actually be reached.
      - As an offshoot, the controller class could not be determined, and thus was
        not returned by the API -- leading to a situation where the "source code"
        tab was unable to produce links to view the controller class (even though
        the class itself existed).

---
+++ test/Model/TestAsset/module/Version/src/Version/V1/Rpc/Ping/PingController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2014 Zend Technologies USA Inc. (http://www.zend.com)
+ */
+
+namespace Version\V1\Ping;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class PingController extends AbstractActionController
+{
+}
