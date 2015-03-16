52e24e99bdc5607f829272e63fabaec11b61f19e
Enrico Zimuel <e.zimuel@gmail.com>
2015-03-16 13:04:48 +0100

    Added package feature

---
+++ src/Controller/PackageController.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2014 Zend Technologies USA Inc. (http://www.zend.com)
+ */
+
+namespace ZF\Apigility\Admin\Controller;
+
+use Zend\Http\Request;
+use Zend\Mvc\Controller\AbstractActionController;
+use ZF\ApiProblem\ApiProblem;
+use ZF\ApiProblem\ApiProblemResponse;
+
+class PackageController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        $request = $this->getRequest();
+
+        switch ($request->getMethod()) {
+            case $request::METHOD_GET:
+                $fileId   = $this->params()->fromQuery('token');
+                $format   = $this->params()->fromQuery('format');
+                $response = $this->getResponse();
+
+                if (!$fileId || !$format) {
+                    $response->setStatusCode(404);
+                    return $response;
+                }
+                $package  = sys_get_temp_dir() . '/apigility_' . $fileId . '.' . strtolower($format);
+                $response = $this->getResponse();
+                $headers  = $response->getHeaders();
+
+                if (!file_exists($package)) {
+                    $response->setStatusCode(404);
+                    return $response;
+                }
+                $fileContents = file_get_contents($package);
+
+                $response->setContent($fileContents);
+                $headers->clearHeaders()
+                        ->addHeaderLine('Content-Type', 'application/octet-stream')
+                        ->addHeaderLine('Content-Disposition', 'attachment; filename="apigility_' .
+                                        date("Y-m-d_H-i-s") . '.' . $format . '"')
+                        ->addHeaderLine('Content-Length', strlen($fileContents));
+
+                unlink($package);
+                return $response;
+                break;
+
+            case $request::METHOD_POST:
+                $format = $this->bodyParam('format', false);
+                if (!$format || !in_array(strtolower($format), array('zip', 'tar', 'tgz', 'zpk'))) {
+                    return new ApiProblemResponse(
+                        new ApiProblem(
+                            422,
+                            'Format parameter not valid, we accept only zip, tar, tgz or zpk type',
+                            'https://tools.ietf.org/html/rfc4918',
+                            'Unprocessable Entity'
+                        )
+                    );
+                }
+                $format   = strtolower($format);
+                $fileId   = uniqid();
+                $package  = sys_get_temp_dir() . '/apigility_' . $fileId . '.' . $format;
+                $cmd      = "php vendor/bin/zfdeploy.php build {$package}";
+
+                $apis = $this->bodyParam('apis');
+                if (!is_null($apis)) {
+                    $modules = array_map(
+                        function ($entry) {
+                            return substr($entry, 7);
+                        },
+                        glob('module/*', GLOB_ONLYDIR)
+                    );
+                    $toInclude = array();
+                    foreach ($modules as $mod) {
+                        if (!isset($apis[$mod]) || $apis[$mod]) {
+                            $toInclude[] = $mod;
+                        }
+                    }
+                    $cmd .= ' --modules=' . escapeshellarg(implode(",", $toInclude));
+                }
+                if (!is_null($this->bodyParam('composer'))) {
+                    $cmd .= $this->bodyParam('composer') ? ' --composer=on' : ' --composer=off';
+                }
+                if (!empty($this->bodyParam('config'))) {
+                    $cmd .= ' --configs=' . escapeshellarg($this->bodyParam('config'));
+                }
+                if ($format === 'zpk') {
+                    if (!is_null($this->bodyParam('zpk_xml'))) {
+                        $cmd .= ' --deploymentxml=' . escapeshellarg($this->bodyParam('zpk_xml'));
+                    }
+                    if (!is_null($this->bodyParam('zpk_assets'))) {
+                        $cmd .= ' --zpkdata=' . escapeshellarg($this->bodyParam('zpk_assets'));
+                    }
+                    if (!is_null($this->bodyParam('zpk_version'))) {
+                        $cmd .= ' --version=' . escapeshellarg($this->bodyParam('zpk_version'));
+                    }
+                }
+
+                // Execute zf-deploy
+                shell_exec($cmd);
+                if (file_exists($package)) {
+                    return array('token' => $fileId, 'format' => $format);
+                }
+                return array();
+
+                break;
+
+            default:
+                return new ApiProblemResponse(
+                    new ApiProblem(405, 'Only the method POST is allowed for this URI')
+                );
+        }
+    }
+
+    /**
+     * Set the request object manually
+     *
+     * Provided for testing.
+     *
+     * @param  Request $request
+     * @return self
+     */
+    public function setRequest(Request $request)
+    {
+        $this->request = $request;
+        return $this;
+    }
+}
---
+++ test/Controller/PackageControllerTest.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2014 Zend Technologies USA Inc. (http://www.zend.com)
+ */
+
+namespace ZFTest\Apigility\Admin\Controller;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use Zend\Http\Request;
+use Zend\Mvc\Controller\PluginManager as ControllerPluginManager;
+use Zend\Stdlib\Parameters;
+use ZF\Apigility\Admin\Controller\PackageController;
+use ZF\ContentNegotiation\ControllerPlugin\BodyParam;
+use Zend\Mvc\MvcEvent;
+use ZF\ContentNegotiation\ParameterDataContainer;
+use Zend\Mvc\Router\RouteMatch;
+
+class PackageControllerTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->controller = new PackageController();
+        $this->plugins = new ControllerPluginManager();
+        $this->plugins->setService('bodyParam', new BodyParam());
+        $this->controller->setPluginManager($this->plugins);
+    }
+
+    public function invalidRequestMethods()
+    {
+        return array(
+            array('patch'),
+            array('put'),
+            array('delete'),
+        );
+    }
+
+    /**
+     * @dataProvider invalidRequestMethods
+     */
+    public function testProcessWithInvalidRequestMethodReturnsApiProblemResponse($method)
+    {
+        $request = new Request();
+        $request->setMethod($method);
+        $this->controller->setRequest($request);
+        $result = $this->controller->indexAction();
+        $this->assertInstanceOf('ZF\ApiProblem\ApiProblemResponse', $result);
+        $apiProblem = $result->getApiProblem();
+        $this->assertEquals(405, $apiProblem->status);
+    }
+
+
+    public function testProcessPostRequestReturnsToken()
+    {
+        $request = new Request();
+        $request->setMethod('post');
+
+        $parameters = new ParameterDataContainer();
+        $parameters->setBodyParam('format', 'ZIP');
+        $event = new MvcEvent();
+        $event->setParam('ZFContentNegotiationParameterData', $parameters);
+
+        $request->getHeaders()->addHeaderLine('Content-Type', 'application/json');
+        $request->getHeaders()->addHeaderLine('Accept', 'application/json');
+
+        $this->controller->setRequest($request);
+        $this->controller->setEvent($event);
+
+        $cwd = getcwd();
+        chdir(__DIR__ . '/TestAsset');
+        $result = $this->controller->indexAction();
+        chdir($cwd);
+
+        $this->assertInternalType('array', $result);
+        $this->assertTrue(isset($result['token']));
+        $this->assertTrue(isset($result['format']));
+        $package = sys_get_temp_dir() . '/apigility_' . $result['token'] . '.' . $result['format'];
+        $this->assertTrue(file_exists($package));
+
+        return $result;
+    }
+
+    /**
+     * @depends testProcessPostRequestReturnsToken
+     */
+    public function testProcessGetRequestReturnsFile($data)
+    {
+        $request = new Request();
+        $request->setMethod('get');
+        $request->getQuery()->set('format', $data['format']);
+        $request->getQuery()->set('token', $data['token']);
+
+        $this->controller->setRequest($request);
+
+        $package = sys_get_temp_dir() . '/apigility_' . $data['token'] . '.' . $data['format'];
+        $content = file_get_contents($package);
+
+        $response = $this->controller->indexAction();
+
+        $this->assertTrue($response->isSuccess());
+        $this->assertEquals($content, (string) $response->getContent());
+        $this->assertEquals('application/octet-stream', $response->getHeaders()->get('Content-Type')->getFieldValue());
+        $this->assertEquals(strlen($content), $response->getHeaders()->get('Content-Length')->getFieldValue());
+        $this->assertTrue(!file_exists($package));
+    }
+}
