e3abacf72da61566e35791a9ff884f324b53db00
Matthew Weier O'Phinney <matthew@zend.com>
2015-08-06 11:41:53 -0500

    Corrected test expectation
    - `required` no longer returns error messages from NotEmpty validator

--- test/InputFilter/CreateContentNegotiationInputFilterTest.php
+++ test/InputFilter/CreateContentNegotiationInputFilterTest.php
@@ -41,7 +41,7 @@ class CreateContentNegotiationInputFilterTest extends TestCase
                 ],
                 [
                     'content_name' => [
-                        'isEmpty' => 'Value is required and can\'t be empty'
+                        'Value is required'
                     ],
                 ],
             ],
