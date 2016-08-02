20ebfcd1b54c95b4abf2c22eb1e92deb46ecc77d
Matthew Weier O'Phinney <matthew@zend.com>
2016-08-02 13:21:21 -0500

    Created tests for AuthorizationControllerFactory

---
+++ test/Factory/AuthorizationControllerFactoryTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2016 Zend Technologies USA Inc. (http://www.zend.com)
+ */
+
+namespace ZF\Apigility\Admin\Factory;
+
+use Interop\Container\ContainerInterface;
+use PHPUnit_Framework_TestCase as TestCase;
+use Zend\ServiceManager\AbstractPluginManager;
+use ZF\Apigility\Admin\Controller\AuthorizationController;
+use ZF\Apigility\Admin\Factory\AuthorizationControllerFactory;
+use ZF\Apigility\Admin\Model\AuthorizationModelFactory;
+
+class AuthorizationControllerFactoryTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->authFactory = $this->prophesize(AuthorizationModelFactory::class)->reveal();
+        $this->container = $this->prophesize(ContainerInterface::class);
+        $this->container->get(AuthorizationModelFactory::class)->willReturn($this->authFactory);
+    }
+
+    public function testInvokableFactoryReturnsAuthorizationController()
+    {
+        $factory = new AuthorizationControllerFactory();
+
+        $controller = $factory($this->container->reveal(), AuthorizationController::class);
+
+        $this->assertInstanceOf(AuthorizationController::class, $controller);
+        $this->assertAttributeSame($this->authFactory, 'factory', $controller);
+    }
+
+    public function testLegacyFactoryReturnsAuthorizationController()
+    {
+        $factory = new AuthorizationControllerFactory();
+        $controllers = $this->prophesize(AbstractPluginManager::class);
+
+        $controllers->getServiceLocator()->will([$this->container, 'reveal']);
+
+        $controller = $factory->createService($controllers->reveal());
+
+        $this->assertInstanceOf(AuthorizationController::class, $controller);
+        $this->assertAttributeSame($this->authFactory, 'factory', $controller);
+    }
+}
