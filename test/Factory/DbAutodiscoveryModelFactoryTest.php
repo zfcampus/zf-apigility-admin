2d1103576f19044b4c17265b6bf40f054a924f11
Matthew Weier O'Phinney <matthew@zend.com>
2016-08-02 14:33:09 -0500

    Provide DbAutodiscoveryModelFactory tests

---
+++ test/Factory/DbAutodiscoveryModelFactoryTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2016 Zend Technologies USA Inc. (http://www.zend.com)
+ */
+
+namespace ZFTest\Apigility\Admin\Factory;
+
+use Interop\Container\ContainerInterface;
+use PHPUnit_Framework_TestCase as TestCase;
+use Zend\ServiceManager\Exception\ServiceNotCreatedException;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use ZF\Apigility\Admin\Factory\DbAutodiscoveryModelFactory;
+use ZF\Apigility\Admin\Model\DbAutodiscoveryModel;
+
+class DbAutodiscoveryModelFactoryTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->container = $this->prophesize(ServiceLocatorInterface::class);
+        $this->container->willImplement(ContainerInterface::class);
+    }
+
+    public function testFactoryRaisesExceptionIfConfigServiceIsMissing()
+    {
+        $factory = new DbAutodiscoveryModelFactory();
+
+        $this->container->has('config')->willReturn(false);
+
+        $this->setExpectedException(ServiceNotCreatedException::class, 'config service is not present');
+        $factory($this->container->reveal());
+    }
+
+    public function testFactoryReturnsDbAutodiscoveryModelComposingConfigAndContainer()
+    {
+        $factory = new DbAutodiscoveryModelFactory();
+        $writer  = $this->prophesize(WriterInterface::class)->reveal();
+
+        $this->container->has('config')->willReturn(true);
+        $this->container->get('config')->willReturn([]);
+
+        $model = $factory($this->container->reveal());
+
+        $this->assertInstanceOf(DbAutodiscoveryModel::class, $model);
+        $this->assertAttributeEquals([], 'config', $model);
+        $this->assertSame($this->container->reveal(), $model->getServiceLocator());
+    }
+}
