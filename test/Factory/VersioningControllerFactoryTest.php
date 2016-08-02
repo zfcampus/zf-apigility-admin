d544ddfee7944462057eb896f79c15c521849b93
Matthew Weier O'Phinney <matthew@zend.com>
2016-08-02 16:20:44 -0500

    Provided VersioningControllerFactory tests

---
+++ test/Factory/VersioningControllerFactoryTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2016 Zend Technologies USA Inc. (http://www.zend.com)
+ */
+
+namespace ZF\Apigility\Admin\Factory;
+
+use Interop\Container\ContainerInterface;
+use PHPUnit_Framework_TestCase as TestCase;
+use Zend\ServiceManager\AbstractPluginManager;
+use ZF\Apigility\Admin\Controller\VersioningController;
+use ZF\Apigility\Admin\Factory\VersioningControllerFactory;
+use ZF\Apigility\Admin\Model\VersioningModelFactory;
+
+class VersioningControllerFactoryTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->model = $this->prophesize(VersioningModelFactory::class)->reveal();
+        $this->container = $this->prophesize(ContainerInterface::class);
+        $this->container->get(VersioningModelFactory::class)->willReturn($this->model);
+    }
+
+    public function testInvokableFactoryReturnsVersioningController()
+    {
+        $factory = new VersioningControllerFactory();
+
+        $controller = $factory($this->container->reveal(), VersioningController::class);
+
+        $this->assertInstanceOf(VersioningController::class, $controller);
+        $this->assertAttributeSame($this->model, 'modelFactory', $controller);
+    }
+
+    public function testLegacyFactoryReturnsVersioningController()
+    {
+        $factory = new VersioningControllerFactory();
+        $controllers = $this->prophesize(AbstractPluginManager::class);
+
+        $controllers->getServiceLocator()->will([$this->container, 'reveal']);
+
+        $controller = $factory->createService($controllers->reveal());
+
+        $this->assertInstanceOf(VersioningController::class, $controller);
+        $this->assertAttributeSame($this->model, 'modelFactory', $controller);
+    }
+}
