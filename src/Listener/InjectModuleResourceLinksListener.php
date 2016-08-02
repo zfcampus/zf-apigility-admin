25f8fa284a1ae0e0176c4f94e6d6d9c3e98af392
Matthew Weier O'Phinney <matthew@zend.com>
2016-08-02 08:17:16 -0500

    Extracted onRender listener to new InjectModuleResourceLinksListener class
    Current tests test only the specific functionality of the `onRender()`
    listener. However, the same listener will also contain the various HAL
    plugin listeners, as they depend on shared state. While those methods
    have been moved into the class, tests have not yet been written for
    them.

---
+++ src/Listener/InjectModuleResourceLinksListener.php
@@ -0,0 +1,449 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2016 Zend Technologies USA Inc. (http://www.zend.com)
+ */
+
+namespace ZF\Apigility\Admin\Listener;
+
+use Interop\Container\ContainerInterface;
+use Zend\Mvc\MvcEvent;
+use Zend\Mvc\Router\RouteMatch as V2RouteMatch;
+use Zend\Router\RouteMatch;
+use ZF\Apigility\Admin\Model;
+use ZF\Hal\Link\Link;
+use ZF\Hal\Link\LinkCollection;
+use ZF\Hal\Entity;
+use ZF\Hal\View\HalJsonModel;
+
+class InjectModuleResourceLinksListener
+{
+    /**
+     * @param RouteMatch|V2RouteMatch
+     */
+    private $routeMatch;
+
+    /**
+     * @param callable
+     */
+    private $urlHelper;
+
+    /**
+     * @var ContainerInterface
+     */
+    private $viewHelpers;
+
+    /**
+     * @param ContainerInterface $viewHelpers
+     */
+    public function __construct(ContainerInterface $viewHelpers)
+    {
+        $this->viewHelpers = $viewHelpers;
+    }
+
+    /**
+     * @param MvcEvent $e
+     * @return void
+     */
+    public function __invoke(MvcEvent $e)
+    {
+        $matches = $e->getRouteMatch();
+        if (! ($matches instanceof RouteMatch || $matches instanceof V2RouteMatch)) {
+            // In 404's, we do not have a route match... nor do we need to do
+            // anything
+            return;
+        }
+
+        $result = $e->getResult();
+        if (! $result instanceof HalJsonModel) {
+            return;
+        }
+
+        $halPlugin = $this->viewHelpers->get('hal');
+        $this->initializeUrlHelper();
+
+        $halPlugin->getEventManager()->attach(
+            ['renderCollection', 'renderEntity', 'renderCollection.Entity'],
+            function ($e) use ($matches) {
+                if ($matches->getParam('controller_service_name')) {
+                    $matches->setParam(
+                        'controller_service_name',
+                        str_replace('\\', '-', $matches->getParam('controller_service_name'))
+                    );
+                }
+            }
+        );
+
+        // If content is empty, then send the response with a 204 and an emtpy body
+
+        if ($result->isEntity()) {
+            $this->injectServiceLinks($result->getPayload(), $result, $e);
+            $halPlugin->getEventManager()->attach('renderEntity', [$this, 'onRenderEntity'], 10);
+            return;
+        }
+
+        if ($result->isCollection()) {
+            $this->routeMatch = $matches;
+            $halPlugin->getEventManager()->attach(
+                'renderCollection.entity',
+                [$this, 'onRenderCollectionEntity'],
+                10
+            );
+        }
+    }
+
+    /**
+     * @param $e
+     */
+    public function onRenderEntity($e)
+    {
+        $halEntity = $e->getParam('entity');
+        $entity    = $halEntity->entity;
+        $hal       = $e->getTarget();
+
+        if ($entity instanceof Model\RestServiceEntity
+            || $entity instanceof Model\RpcServiceEntity
+            || (is_array($entity) && array_key_exists('controller_service_name', $entity))
+        ) {
+            $serviceName = is_array($entity) ? $entity['controller_service_name'] : $entity->controllerServiceName;
+            $links       = $halEntity->getLinks();
+
+            if ($links->has('input_filter')) {
+                $link   = $links->get('input_filter');
+                $params = $link->getRouteParams();
+                $link->setRouteParams(array_merge($params, [
+                    'controller_service_name' => $serviceName,
+                ]));
+            }
+
+            if ($links->has('documentation')) {
+                $link   = $links->get('documentation');
+                $params = $link->getRouteParams();
+                $link->setRouteParams(array_merge($params, [
+                    'controller_service_name' => $serviceName,
+                ]));
+            }
+
+            if (! $links->has('self')) {
+                $route  = 'zf-apigility/api/module/';
+                $route .= $entity instanceof Model\RestServiceEntity ? 'rest-service' : 'rpc-service';
+                $hal->injectSelfLink($halEntity, $route, 'controller_service_name');
+            }
+            return;
+        }
+
+        if ($entity instanceof Model\InputFilterEntity
+            || (is_array($entity) && isset($entity['input_filter_name']))
+        ) {
+            switch (true) {
+                case ($entity instanceof Model\RestInputFilterEntity):
+                    $type = 'rest-service';
+                    break;
+                case ($entity instanceof Model\RpcInputFilterEntity):
+                    $type = 'rpc-service';
+                    break;
+            }
+            $links = $halEntity->getLinks();
+
+            if (! $links->has('self')) {
+                $route = sprintf('zf-apigility/api/module/%s/input-filter', $type);
+                $hal->injectSelfLink($halEntity, $route, 'input_filter_name');
+            }
+            return;
+        }
+    }
+
+    /**
+     * Inject links into collections
+     *
+     * Currently:
+     *
+     * - Inject RPC/REST service links inside module resources that are
+     *   composed in collections
+     *
+     * @param  \Zend\EventManager\Event $e
+     */
+    public function onRenderCollectionEntity($e)
+    {
+        $entity = $e->getParam('entity');
+        if ($entity instanceof Model\ModuleEntity) {
+            return $this->injectModuleCollectionRelationalLinks($entity, $e);
+        }
+
+        if ($entity instanceof Model\RestServiceEntity
+            || $entity instanceof Model\RpcServiceEntity
+        ) {
+            return $this->injectServiceCollectionRelationalLinks($entity, $e);
+        }
+
+        if ($entity instanceof Model\InputFilterEntity) {
+            return $this->normalizeInputFilterEntityName($entity, $e);
+        }
+    }
+
+    /**
+     * Inject relational links into a Module resource
+     *
+     * @param Model\ModuleEntity $resource
+     * @param \Zend\Mvc\MvcEvent $e
+     */
+    private function injectModuleCollectionRelationalLinks(Model\ModuleEntity $resource, $e)
+    {
+        $asArray = $resource->getArrayCopy();
+        $module  = $asArray['name'];
+        $rest    = $asArray['rest'];
+        $rpc     = $asArray['rpc'];
+
+        unset($asArray['rest']);
+        unset($asArray['rpc']);
+
+        $halEntity = new Entity($asArray, $module);
+        $links     = $halEntity->getLinks();
+        $links->add(Link::factory([
+            'rel' => 'self',
+            'route' => [
+                'name' => 'zf-apigility/api/module',
+                'params' => [
+                    'name' => $module,
+                ],
+            ],
+        ]));
+        $this->injectLinksByType('authorization', $links, $module);
+        $this->injectLinksByType('rest', $links, $module);
+        $this->injectLinksByType('rpc', $links, $module);
+
+        $e->setParam('entity', $halEntity);
+    }
+
+    /**
+     * @param $entity
+     * @param $e
+     */
+    private function injectServiceCollectionRelationalLinks($entity, $e)
+    {
+        $entity->exchangeArray([
+            'controller_service_name' => str_replace('\\', '-', $entity->controllerServiceName),
+        ]);
+
+        $module  = $this->routeMatch->getParam('name');
+        $service = $entity->controllerServiceName;
+        $type    = $this->getServiceType($service);
+
+        $halEntity = new Entity($entity, $service);
+        $links = $halEntity->getLinks();
+
+        // Need to inject the self relational link, as otherwise the HAL plugin
+        // sees we have links, and does not inject one.
+        $links->add(Link::factory([
+            'rel' => 'self',
+            'route' => [
+                'name' => sprintf('zf-apigility/api/module/%s-service', $type),
+                'params' => [
+                    'name' => $module,
+                    'controller_service_name' => $service,
+                ],
+            ],
+        ]));
+
+        // Add the input_filter relational link
+        $links->add(Link::factory([
+            'rel' => 'input_filter',
+            'route' => [
+                'name' => sprintf('zf-apigility/api/module/%s-service/input-filter', $type),
+                'params' => [
+                    'name' => $module,
+                    'controller_service_name' => $service,
+                ],
+            ],
+        ]));
+
+        // Add the documentation relational link
+        $links->add(Link::factory([
+            'rel' => 'documentation',
+            'route' => [
+                'name' => sprintf('zf-apigility/api/module/%s-service/doc', $type),
+                'params' => [
+                    'name' => $module,
+                    'controller_service_name' => $service,
+                ],
+            ],
+        ]));
+
+        $e->setParam('entity', $halEntity);
+    }
+
+    private function initializeUrlHelper()
+    {
+        $urlHelper       = $this->viewHelpers->get('Url');
+        $serverUrlHelper = $this->viewHelpers->get('ServerUrl');
+
+        $this->urlHelper = function (
+            $routeName,
+            $routeParams,
+            $routeOptions,
+            $reUseMatchedParams
+        ) use (
+            $urlHelper,
+            $serverUrlHelper
+        ) {
+            $url = call_user_func(
+                $urlHelper,
+                $routeName,
+                $routeParams,
+                $routeOptions,
+                $reUseMatchedParams
+            );
+
+            if (substr($url, 0, 4) == 'http') {
+                return $url;
+            }
+            return call_user_func($serverUrlHelper, $url);
+        };
+    }
+
+    /**
+     * Inject links for the service services of a module
+     *
+     * @param Entity $halEntity
+     * @param HalJsonModel $model
+     * @param $e
+     */
+    private function injectServiceLinks(Entity $halEntity, HalJsonModel $model, $e)
+    {
+        $entity = $halEntity->getEntity();
+        $links  = $halEntity->getLinks();
+        if ($entity instanceof Model\ModuleEntity) {
+            $this->injectModuleResourceRelationalLinks($entity, $links, $model);
+        }
+        if ($entity instanceof Model\RestServiceEntity || $entity instanceof Model\RpcServiceEntity) {
+            $this->normalizeEntityControllerServiceName($entity, $links, $model);
+        }
+        if ($entity instanceof Model\InputFilterEntity) {
+            $this->normalizeEntityInputFilterName($entity, $links, $model);
+        }
+    }
+
+    /**
+     * @param Model\ModuleEntity $module
+     * @param $links
+     * @param HalJsonModel $model
+     */
+    private function injectModuleResourceRelationalLinks(Model\ModuleEntity $module, $links, HalJsonModel $model)
+    {
+        $moduleData = $module->getArrayCopy();
+        $moduleName = $moduleData['name'];
+
+        $this->injectLinksByType('authorization', $links, $moduleName);
+
+        $this->injectLinksByType('rest', $links, $moduleName);
+        unset($moduleData['rest']);
+
+        $this->injectLinksByType('rpc', $links, $moduleName);
+        unset($moduleData['rpc']);
+
+        $module = new Model\ModuleEntity($module->getNamespace(), [], [], $module->isVendor());
+        $module->exchangeArray($moduleData);
+        $replacement = new Entity($module, $moduleName);
+        $replacement->setLinks($links);
+        $model->setPayload($replacement);
+    }
+
+    /**
+     * @param $entity
+     * @param $links
+     * @param HalJsonModel $model
+     */
+    private function normalizeEntityControllerServiceName($entity, $links, HalJsonModel $model)
+    {
+        $entity->exchangeArray([
+            'controller_service_name' => str_replace('\\', '-', $entity->controllerServiceName),
+        ]);
+        $halEntity = new Entity($entity, $entity->controllerServiceName);
+
+        if ($links->has('self')) {
+            $links->remove('self');
+        }
+        $halEntity->setLinks($links);
+
+        $model->setPayload($halEntity);
+    }
+
+    /**
+     * @param Model\InputFilterEntity $entity
+     * @param $links
+     * @param HalJsonModel $model
+     */
+    private function normalizeEntityInputFilterName(Model\InputFilterEntity $entity, $links, HalJsonModel $model)
+    {
+        $entity['input_filter_name'] = str_replace('\\', '-', $entity['input_filter_name']);
+        $halEntity = new Entity($entity, $entity['input_filter_name']);
+
+        if ($links->has('self')) {
+            $links->remove('self');
+        }
+        $halEntity->setLinks($links);
+
+        $model->setPayload($halEntity);
+    }
+
+    /**
+     * @param $entity
+     * @param $e
+     */
+    private function normalizeInputFilterEntityName($entity, $e)
+    {
+        $entity['input_filter_name'] = str_replace('\\', '-', $entity['input_filter_name']);
+        $e->setParam('entity', $entity);
+    }
+
+    /**
+     * Inject service links
+     *
+     * @param  string $type "rpc" | "rest" | "authorization"
+     * @param  LinkCollection $links
+     * @param  null|string $module
+     */
+    private function injectLinksByType($type, LinkCollection $links, $module = null)
+    {
+        $urlHelper = $this->urlHelper;
+
+        $linkType = $type;
+        if (in_array($type, ['rpc', 'rest'])) {
+            $linkType .= '-service';
+        }
+        $routeName = sprintf('zf-apigility/api/module/%s', $linkType);
+
+        $routeParams = [];
+        if (null !== $module) {
+            $routeParams['name'] = $module;
+        }
+
+        $routeOptions = [];
+
+        $url  = call_user_func($urlHelper, $routeName, $routeParams, $routeOptions, false);
+        $url .= '{?version}';
+
+        $spec = [
+            'rel'   => $type,
+            'url'   => $url,
+            'props' => [
+                'templated' => true,
+            ],
+        ];
+
+        $link = Link::factory($spec);
+        $links->add($link);
+    }
+
+    /**
+     * @param $service
+     * @return string
+     */
+    private function getServiceType($service)
+    {
+        if (preg_match('#[\\-.]Rest[\\-.]#', $service)) {
+            return 'rest';
+        }
+        return 'rpc';
+    }
+}
---
+++ test/Listener/InjectModuleResourceLinksListenerTest.php
@@ -0,0 +1,368 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2016 Zend Technologies USA Inc. (http://www.zend.com)
+ */
+
+namespace ZFTest\Apigility\Admin\Listener;
+
+use Closure;
+use Interop\Container\ContainerInterface;
+use PHPUnit_Framework_TestCase as TestCase;
+use Prophecy\Argument;
+use stdClass;
+use Zend\EventManager\EventManagerInterface;
+use Zend\Mvc\MvcEvent;
+use ZF\Apigility\Admin\Listener\InjectModuleResourceLinksListener;
+use ZF\Apigility\Admin\Model\InputFilterEntity;
+use ZF\Apigility\Admin\Model\ModuleEntity;
+use ZF\Apigility\Admin\Model\RestServiceEntity;
+use ZF\Apigility\Admin\Model\RpcServiceEntity;
+use ZF\Hal\Entity;
+use ZF\Hal\Link\Link;
+use ZF\Hal\Link\LinkCollection;
+use ZF\Hal\Plugin\Hal;
+use ZF\Hal\View\HalJsonModel;
+use ZFTest\Apigility\Admin\RouteAssetsTrait;
+use ZFTest\Apigility\Admin\TestAsset\Closure as MockClosure;
+
+class InjectModuleResourceLinksListenerTest extends TestCase
+{
+    use RouteAssetsTrait;
+
+    public function setUp()
+    {
+        $this->event           = $this->prophesize(MvcEvent::class);
+        $this->routeMatch      = $this->prophesize($this->getRouteMatchClass());
+        $this->events          = $this->prophesize(EventManagerInterface::class);
+        $this->hal             = $this->prophesize(Hal::class);
+        $this->result          = $this->prophesize(HalJsonModel::class);
+        $this->urlHelper       = $this->prophesize(MockClosure::class);
+        $this->serverUrlHelper = $this->prophesize(MockClosure::class);
+
+        $this->helpers         = $this->prophesize(ContainerInterface::class);
+        $this->listener        = new InjectModuleResourceLinksListener($this->helpers->reveal());
+    }
+
+    public function initRequiredConditions()
+    {
+        $this->event->getRouteMatch()->will([$this->routeMatch, 'reveal'])->shouldBeCalled();
+        $this->event->getResult()->will([$this->result, 'reveal']);
+
+        $this->helpers->get('hal')->will([$this->hal, 'reveal']);
+        $this->helpers->get('Url')->willReturn(function (...$args) {
+            $helper = $this->urlHelper->reveal();
+            return $helper->call(...$args);
+        });
+        $this->helpers->get('ServerUrl')->willReturn(function (...$args) {
+            $helper = $this->serverUrlHelper->reveal();
+            return $helper->call(...$args);
+        });
+
+        $this->events
+            ->attach(
+                [
+                    'renderCollection',
+                    'renderEntity',
+                    'renderCollection.Entity'
+                ],
+                Argument::type(Closure::class)
+            )
+            ->shouldBeCalled();
+        $this->hal->getEventManager()->will([$this->events, 'reveal']);
+    }
+
+    public function testListenerDoesNothingIfEventHasNoRouteMatch()
+    {
+        $listener = $this->listener;
+
+        $this->event->getRouteMatch()->willReturn(null)->shouldBeCalled();
+        $this->event->getResult()->shouldNotBeCalled();
+        $this->helpers->get('hal')->shouldNotBeCalled();
+
+        $this->assertNull($listener($this->event->reveal()));
+        $this->assertAttributeEmpty('urlHelper', $listener);
+    }
+
+    public function testListenerDoesNothingIfResultIsNotAHalJsonModel()
+    {
+        $listener = $this->listener;
+
+        $this->event->getRouteMatch()->will([$this->routeMatch, 'reveal'])->shouldBeCalled();
+        $this->event->getResult()->willReturn(new stdClass());
+        $this->helpers->get('hal')->shouldNotBeCalled();
+
+        $this->assertNull($listener($this->event->reveal()));
+        $this->assertAttributeEmpty('urlHelper', $listener);
+    }
+
+    public function testRegistersAlternateUrlHelperAndAttachesHalPluginListenersIfHalJsonModelDetected()
+    {
+        $listener = $this->listener;
+        $this->initRequiredConditions();
+
+        $this->result->isEntity()->willReturn(false)->shouldBeCalled();
+        $this->result->isCollection()->willReturn(false)->shouldBeCalled();
+
+        $this->assertNull($listener($this->event->reveal()));
+        $this->assertAttributeInstanceOf(Closure::class, 'urlHelper', $listener);
+    }
+
+    public function testInjectsModuleEntityWithModuleResourceRelationalLinksAndAttachesRenderEntityListener()
+    {
+        $moduleData = [
+            'name'          => 'FooConf',
+            'authorization' => [],
+            'rest'          => ['ignored'],
+            'rpc'           => ['ignored'],
+        ];
+        $listener   = $this->listener;
+        $module     = $this->prophesize(ModuleEntity::class);
+        $links      = $this->prophesize(LinkCollection::class);
+        $payload    = new Entity($module->reveal(), 'FooConf');
+        $payload->setLinks($links->reveal());
+        $this->initRequiredConditions();
+
+        $this->result->isEntity()->willReturn(true)->shouldBeCalled();
+        $this->result->getPayload()->willReturn($payload);
+
+        $module->getArrayCopy()->willReturn($moduleData);
+        $module->getNamespace()->willReturn('FooConf');
+        $module->isVendor()->willReturn(false);
+
+        $this->result
+            ->setPayload(Argument::that(function ($entity) use ($moduleData, $links) {
+                if (! $entity instanceof Entity) {
+                    return false;
+                }
+
+                if (! $links->reveal() === $entity->getLinks()) {
+                    return false;
+                }
+
+                $moduleEntity = $entity->getEntity();
+                if (! $moduleEntity instanceof ModuleEntity) {
+                    return false;
+                }
+
+                if ($moduleData['name'] !== $moduleEntity->getNamespace()) {
+                    return false;
+                }
+
+                $data = $moduleEntity->getArrayCopy();
+                if (! empty($data['rest'])) {
+                    return false;
+                }
+                if (! empty($data['rpc'])) {
+                    return false;
+                }
+                if (isset($data['authorization'])) {
+                    return false;
+                }
+
+                return true;
+            }))
+            ->shouldBeCalled();
+
+        $this->urlHelper
+            ->call(
+                'zf-apigility/api/module/authorization',
+                [ 'name' => 'FooConf' ],
+                [],
+                false
+            )
+            ->willReturn('/zf-apigility/api/module/authorization');
+        $this->serverUrlHelper
+            ->call('/zf-apigility/api/module/authorization')
+            ->willReturn('http://localhost/zf-apigility/api/module/authorization');
+
+        $this->urlHelper
+            ->call(
+                'zf-apigility/api/module/rest-service',
+                [ 'name' => 'FooConf' ],
+                [],
+                false
+            )
+            ->willReturn('/zf-apigility/api/module/rest-service');
+        $this->serverUrlHelper
+            ->call('/zf-apigility/api/module/rest-service')
+            ->willReturn('http://localhost/zf-apigility/api/module/rest-service');
+
+        $this->urlHelper
+            ->call(
+                'zf-apigility/api/module/rpc-service',
+                [ 'name' => 'FooConf' ],
+                [],
+                false
+            )
+            ->willReturn('/zf-apigility/api/module/rpc-service');
+        $this->serverUrlHelper
+            ->call('/zf-apigility/api/module/rpc-service')
+            ->willReturn('http://localhost/zf-apigility/api/module/rpc-service');
+
+        $links->add(Argument::type(Link::class))->shouldBeCalledTimes(3);
+
+        $this->events
+            ->attach(
+                'renderEntity',
+                [$listener, 'onRenderEntity'],
+                10
+            )
+            ->shouldBeCalled();
+
+        $this->result->isCollection()->willReturn(false)->shouldNotBeCalled();
+
+        $this->assertNull($listener($this->event->reveal()));
+    }
+
+    public function serviceEntities()
+    {
+        return [
+            RestServiceEntity::class => [RestServiceEntity::class],
+            RpcServiceEntity::class  => [RpcServiceEntity::class],
+        ];
+    }
+
+    /**
+     * @dataProvider serviceEntities
+     */
+    public function testUpdatesServiceEntityWithNormalizedControllerNameAndAttachesRenderEntityListener($entityType)
+    {
+        $listener = $this->listener;
+        $links    = $this->prophesize(LinkCollection::class);
+
+        $serviceEntity = new $entityType();
+        $serviceEntity->exchangeArray([
+            'controller_service_name'  => 'Version\V1\Rest\Foo',
+        ]);
+
+        $payload  = new Entity($serviceEntity, 'Version\V1\Rest\Foo');
+        $payload->setLinks($links->reveal());
+        $this->initRequiredConditions();
+
+        $this->result->isEntity()->willReturn(true)->shouldBeCalled();
+        $this->result->getPayload()->willReturn($payload);
+
+        $links->has('self')
+            ->willReturn(true)
+            ->shouldBeCalled();
+        $links->remove('self')->shouldBeCalled();
+
+        $this->result
+            ->setPayload(Argument::that(function ($entity) use ($entityType, $serviceEntity, $links) {
+                if (! $entity instanceof Entity) {
+                    return false;
+                }
+
+                if ($entity->getId() !== 'Version-V1-Rest-Foo') {
+                    return false;
+                }
+
+                if (! $links->reveal() === $entity->getLinks()) {
+                    return false;
+                }
+
+                $foundEntity = $entity->getEntity();
+                if (! $foundEntity instanceof $entityType) {
+                    return false;
+                }
+
+                if ($foundEntity->controllerServiceName !== 'Version-V1-Rest-Foo') {
+                    return false;
+                }
+
+                return true;
+            }))
+            ->shouldBeCalled();
+
+        $this->events
+            ->attach(
+                'renderEntity',
+                [$listener, 'onRenderEntity'],
+                10
+            )
+            ->shouldBeCalled();
+
+        $this->result->isCollection()->willReturn(false)->shouldNotBeCalled();
+
+        $this->assertNull($listener($this->event->reveal()));
+    }
+
+    public function testUpdatesInputFilterEntityWithNormalizedInputFilterNameAndAttachesRenderEntityListener()
+    {
+        $listener = $this->listener;
+        $links    = $this->prophesize(LinkCollection::class);
+
+        $serviceEntity = new InputFilterEntity();
+        $serviceEntity->exchangeArray([
+            'input_filter_name'  => 'Version\V1\Rest\Foo\InputFilter',
+        ]);
+
+        $payload  = new Entity($serviceEntity, 'Version\V1\Rest\Foo\InputFilter');
+        $payload->setLinks($links->reveal());
+        $this->initRequiredConditions();
+
+        $this->result->isEntity()->willReturn(true)->shouldBeCalled();
+        $this->result->getPayload()->willReturn($payload);
+
+        $links->has('self')
+            ->willReturn(true)
+            ->shouldBeCalled();
+        $links->remove('self')->shouldBeCalled();
+
+        $this->result
+            ->setPayload(Argument::that(function ($entity) use ($serviceEntity, $links) {
+                if (! $entity instanceof Entity) {
+                    return false;
+                }
+
+                if ($entity->getId() !== 'Version-V1-Rest-Foo-InputFilter') {
+                    return false;
+                }
+
+                if (! $links->reveal() === $entity->getLinks()) {
+                    return false;
+                }
+
+                $foundEntity = $entity->getEntity();
+                if (! $foundEntity instanceof InputFilterEntity) {
+                    return false;
+                }
+
+                if ($foundEntity['input_filter_name'] !== 'Version-V1-Rest-Foo-InputFilter') {
+                    return false;
+                }
+
+                return true;
+            }))
+            ->shouldBeCalled();
+
+        $this->events
+            ->attach(
+                'renderEntity',
+                [$listener, 'onRenderEntity'],
+                10
+            )
+            ->shouldBeCalled();
+
+        $this->result->isCollection()->willReturn(false)->shouldNotBeCalled();
+
+        $this->assertNull($listener($this->event->reveal()));
+    }
+
+    public function testMemoizesRouteMatchAndAttachesRenderCollectionEntityListener()
+    {
+        $listener = $this->listener;
+        $this->initRequiredConditions();
+
+        $this->result->isEntity()->willReturn(false)->shouldBeCalled();
+        $this->result->isCollection()->willReturn(true)->shouldBeCalled();
+
+        $this->events
+            ->attach('renderCollection.entity', [$listener, 'onRenderCollectionEntity'], 10)
+            ->shouldBeCalled();
+
+        $this->assertNull($listener($this->event->reveal()));
+        $this->assertAttributeSame($this->routeMatch->reveal(), 'routeMatch', $listener);
+    }
+}
